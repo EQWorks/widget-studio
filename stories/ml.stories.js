4be5a37eef361a9c48bef62eb937cfdb4c352ed1
Runzhou Li (woozyking) <runzhou.li@gmail.com>
2020-08-31 18:13:00 -0400

    devops - working storybook

--- stories/ml.stories.js
+++ stories/ml.stories.js
@@ -6,7 +6,10 @@ import axios from 'axios'
 import ML, { FO } from '../src'
 
 const api = axios.create({
-  baseURL: `${process.env.API_HOST}/${process.env.API_STAGE}`,
+  baseURL: [
+    process.env.API_HOST || 'http://localhost:3000',
+    process.env.API_STAGE,
+  ].filter(v => v).join('/'),
   headers: { 'eq-api-jwt': process.env.JWT },
 })
 const actions = Object.entries(FO(api)).reduce((acc, [name, fn]) => {
