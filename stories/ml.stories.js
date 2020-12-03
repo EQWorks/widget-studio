2beb0dfd3f5707d2d35067d847a8205edc479c87
hyx131 <selin.hyx@gmail.com>
2020-12-03 17:45:05 -0500

    login - get jwt from localStorage & resume gh-pages deployment [build]

--- stories/ml.stories.js
+++ stories/ml.stories.js
@@ -1,29 +1,8 @@
 import React from 'react'
-import axios from 'axios'
-
-import { action } from '@storybook/addon-actions'
-
 import { LoginContextProvider } from '@eqworks/common-login'
 
-// import ML, { FO } from '../src'
-import { FO } from '../src'
 import AuthML from '../src/auth-ml'
 
-const api = axios.create({
-  baseURL: [
-    process.env.STORYBOOK_API_HOST || 'http://localhost:3000',
-    process.env.STORYBOOK_API_STAGE,
-  ].filter(v => v).join('/'),
-  headers: { 'eq-api-jwt': process.env.STORYBOOK_JWT },
-})
-const actions = Object.entries(FO(api)).reduce((acc, [name, fn]) => {
-  // inject storybook action call to all actions
-  acc[name] = (...args) => {
-    action(name)
-    return fn(...args)
-  }
-  return acc
-}, {})
 
 export default {
   title: 'LOCUS ML',
@@ -32,13 +11,12 @@ export default {
 
 export const normal = () => (
   <LoginContextProvider>
-    <AuthML actions={actions} />
+    <AuthML />
   </LoginContextProvider>
 )
 export const normalWithDefaultView = () => (
   <LoginContextProvider>
     <AuthML
-      actions={actions}
       defaultView={{
         type: 'layer',
         id: 'layer_1124_1'
