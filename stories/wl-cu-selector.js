665b5c02cbd73d79a38a014ca9adba75836c9347
hyx131 <selin.hyx@gmail.com>
2021-04-14 20:34:21 -0400

    devops - include fallback api_stage

--- stories/wl-cu-selector.js
+++ stories/wl-cu-selector.js
@@ -29,7 +29,7 @@ const useStyles = makeStyles((theme) => ({
 const api = axios.create({
   baseURL: [
     process.env.API_HOST || process.env.STORYBOOK_API_HOST || 'http://localhost:3000',
-    process.env.STORYBOOK_API_STAGE,
+    process.env.API_STAGE || process.env.STORYBOOK_API_STAGE,
   ].filter(v => v).join('/'),
   headers: { 'eq-api-jwt': window.localStorage.getItem('auth_jwt') },
 })
