06f314cf3e39fd38767f5841c6d114e2aa019fbf
Erika Szabo <erika.szabo.geo@gmail.com>
2021-10-21 14:13:16 -0400

    constants - create file for app const and add coordKeys const

---
+++ src/constants.js
@@ -0,0 +1,14 @@
+export const coordKeys = [
+  'lat',
+  'lon',
+  'lng',
+  'latitude',
+  'longitude',
+  'poi_lat',
+  'poi_lon',
+  'source_lat',
+  'source_lon',
+  'target_lat',
+  'target_lon',
+  'geometry',
+]
