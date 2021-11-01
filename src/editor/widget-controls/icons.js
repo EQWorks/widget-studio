483accb36050d20ff742a56841d3c03807ebe45e
Erika Szabo <erika.szabo.geo@gmail.com>
2021-11-01 18:48:41 -0400

    WidgetControls/Icons - define mapIconAvailability for mapIcons to be used with MapIcon comp

--- src/editor/widget-controls/icons.js
+++ src/editor/widget-controls/icons.js
@@ -9,19 +9,31 @@ import InsertChartIcon from '@material-ui/icons/InsertChart'
 import ScatterPlotIcon from '@material-ui/icons/ScatterPlot'
 
 import { useStoreActions, useStoreState } from '../../store'
+import { MAP_LAYER_GEO_KEYS, COORD_KEYS } from '../../constants/map'
 
 
-const mapIcons = [
-  { type: 'map', Component: MapIcon, available: false, cat: ['Geometry'], minAxis: 2 },
-  { type: 'pie', Component: PieChartIcon, available: true, cat: ['Numeric', 'String'], minAxis: 2 },
-  { type: 'bar', Component: InsertChartIcon, available: true, cat: ['Numeric', 'String', 'Date'], minAxis: 2 },
-  { type: 'scatter', Component: ScatterPlotIcon, available: true, cat: ['Num', 'String', 'Date'], minAxis: 2 },
-  { type: 'line', Component: TimelineIcon, available: true, cat: ['Date', 'Numeric', 'String'], minAxis: 2 },
-]
 const Icons = ({ disabled }) => {
-
   const updateStore = useStoreActions((actions) => actions.update)
   const current = useStoreState((state) => state.type)
+  const groupKey = useStoreState((state) => state.groupKey)
+  const rows = useStoreState((state) => state.rows)
+
+  const dataKeys = rows?.length ? Object.keys(rows[0]) : null
+  // TO DO (ERIKA) - this is just temporary for scatterplot and available mvt tile layers
+  // in the future we implement this with a complex validation process for specific geometry keys
+  const mapIconAvailability = (MAP_LAYER_GEO_KEYS.scatterplot.includes(groupKey) &&
+      dataKeys?.some(key => COORD_KEYS.latitude.includes(key)) &&
+      dataKeys?.some(key => COORD_KEYS.longitude.includes(key))) ||
+    MAP_LAYER_GEO_KEYS.MVT.includes(groupKey)
+
+  const mapIcons = [
+    { type: 'map', Component: MapIcon, available: mapIconAvailability, cat: ['Geometry'], minAxis: 2 },
+    { type: 'pie', Component: PieChartIcon, available: true, cat: ['Numeric', 'String'], minAxis: 2 },
+    { type: 'bar', Component: InsertChartIcon, available: true, cat: ['Numeric', 'String', 'Date'], minAxis: 2 },
+    { type: 'scatter', Component: ScatterPlotIcon, available: true, cat: ['Num', 'String', 'Date'], minAxis: 2 },
+    { type: 'line', Component: TimelineIcon, available: true, cat: ['Date', 'Numeric', 'String'], minAxis: 2 },
+  ]
+
   return (
     mapIcons.map(({ Component, type, available }, i) => {
       return (
