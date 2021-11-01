b3e36b1bc2e2741db0c110b5f8ad3c147f33bca2
Erika Szabo <erika.szabo.geo@gmail.com>
2021-11-01 18:50:05 -0400

    MapWidget - replace valueKeys with mapValueKeys for MapWidget comp

--- src/view/adapter/adapters/react-maps.js
+++ src/view/adapter/adapters/react-maps.js
@@ -50,50 +50,53 @@ const getKeyValues = (objArray, key) => objArray.filter(o => o.map_vis === key)[
 
 export default {
   component: Map,
-  adapt: (data, { options, ...config }) => ({
-    // create a good id
-    dataConfig: [{ id: 'testWIReport', data }],
-    layerConfig: [{
-      layer: 'scatterplot',
-      dataId: 'testWIReport',
-      geometry: { longitude: 'lon', latitude: 'lat' },
-      visualizations: {
-        [mapVis.RADIUS]: config?.valueKeys ?
-          {
-            value: {
-              field: `${getKeyValues(config.valueKeys, mapVis.RADIUS)?.key}_${getKeyValues(config.valueKeys, mapVis.RADIUS)?.agg}`,
+  adapt: (data, { options, ...config }) => {
+    const { mapValueKeys } = config
+    return ({
+      // create a good id
+      dataConfig: [{ id: 'testWIReport', data }],
+      layerConfig: [{
+        layer: 'scatterplot',
+        dataId: 'testWIReport',
+        geometry: { longitude: 'lon', latitude: 'lat' },
+        visualizations: {
+          radius: config?.mapValueKeys ?
+            {
+              value: {
+                field: `${getKeyValues(mapValueKeys, mapVis.RADIUS)?.key}_${getKeyValues(mapValueKeys, mapVis.RADIUS)?.agg}`,
+              },
+              valueOptions: [5, 15],
+              dataScale: 'linear',
+            }:
+            null,
+          fill: getKeyValues(config?.mapValueKeys, mapVis.FILL)?.key ?
+            {
+              value: {
+                field: `${getKeyValues(mapValueKeys, mapVis.FILL)?.key}_${getKeyValues(mapValueKeys, mapVis.FILL)?.agg}`,
+              },
+              valueOptions: [[214, 232, 253], [39, 85, 196]],
+              dataScale: 'linear',
+            }:
+            null,
+        },
+        interactions: {
+          tooltip: {
+            tooltipKeys: {
+              name: 'poi_name',
             },
-            valueOptions: [5, 15],
-            dataScale: 'linear',
-          }:
-          null,
-        [mapVis.FILL]: getKeyValues(config?.valueKeys, mapVis.FILL)?.key ?
-          {
-            value: {
-              field: `${getKeyValues(config.valueKeys, mapVis.FILL)?.key}_${getKeyValues(config.valueKeys, mapVis.FILL)?.agg}`,
-            },
-            valueOptions: [[214, 232, 253], [39, 85, 196]],
-            dataScale: 'linear',
-          }:
-          null,
-      },
-      interactions: {
-        tooltip: {
-          tooltipKeys: {
-            name: 'poi_name',
           },
         },
+        legend: { showLegend: true },
+        opacity: 0.3,
+      }],
+      mapConfig: {
+        cursor: (layers) => getCursor({ layers }),
+        legendPosition: 'top-right',
+        legendSize: 'widget',
+        mapboxApiAccessToken: process.env.MAPBOX_ACCESS_TOKEN || process.env.STORYBOOK_MAPBOX_ACCESS_TOKEN, // <ignore scan-env>
+        showMapLegend: options.showLegend,
+        showMapTooltip: options.showTooltip,
       },
-      legend: { showLegend: true },
-      opacity: 0.3,
-    }],
-    mapConfig: {
-      cursor: (layers) => getCursor({ layers }),
-      legendPosition: 'top-right',
-      legendSize: 'widget',
-      mapboxApiAccessToken: process.env.MAPBOX_ACCESS_TOKEN || process.env.STORYBOOK_MAPBOX_ACCESS_TOKEN, // <ignore scan-env>
-      showMapLegend: options.showLegend,
-      showMapTooltip: options.showTooltip,
-    },
-  }),
+    })
+  },
 }
