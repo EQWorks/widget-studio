c2f44b04ed84a0c50ff445ed621ea0d43b855948
Erika Szabo <erika.szabo.geo@gmail.com>
2021-11-06 09:58:56 -0400

    adapters/react-maps - infer layer config obj for map from state and prop values

--- src/view/adapter/adapters/react-maps.js
+++ src/view/adapter/adapters/react-maps.js
@@ -7,7 +7,14 @@ import { LocusMap } from '@eqworks/react-maps'
 import { getCursor } from '@eqworks/react-maps/dist/utils'
 import { makeStyles } from '@material-ui/core/styles'
 import modes from '../../../constants/modes'
-import { mapVis } from '../../../constants/map'
+import {
+  COORD_KEYS,
+  MAP_LAYERS,
+  MAP_LAYER_VIS,
+  MAP_LAYER_GEO_KEYS,
+  VIS_OPTIONS,
+  OPACITY,
+} from '../../../constants/map'
 
 
 const useStyles = makeStyles({
@@ -46,48 +53,55 @@ Map.propTypes = {
   props: PropTypes.object.isRequired,
 }
 
-const getKeyValues = (objArray, key) => objArray.filter(o => o.map_vis === key)[0]
-
 export default {
   component: Map,
   adapt: (data, { options, ...config }) => {
-    const { mapValueKeys } = config
+    const { groupKey, mapValueKeys } = config
+    const mapLayer = Object.keys(MAP_LAYER_VIS).filter(layer => MAP_LAYER_GEO_KEYS[layer].includes(groupKey))[0]
+    //----TO DO - extend geometry logic for other layers if necessary
+    const dataKeys = Object.keys(data[0])
+
+    const getTooltipKey = (tooltipKey) =>
+      dataKeys.filter(key => MAP_LAYER_GEO_KEYS[mapLayer].includes(key) && key.includes(tooltipKey))[0]
+
+    const name = getTooltipKey('name')
+    const id = getTooltipKey('id')
+
+    let geometry = {}
+    if (mapLayer === MAP_LAYERS.scatterplot) {
+      const latitude = dataKeys.filter(key => COORD_KEYS.latitude.includes(key))[0]
+      const longitude = dataKeys.filter(key => COORD_KEYS.longitude.includes(key))[0]
+      geometry = { longitude, latitude }
+    }
+
     return ({
       // create a good id
       dataConfig: [{ id: 'testWIReport', data }],
       layerConfig: [{
-        layer: 'scatterplot',
+        layer: mapLayer,
         dataId: 'testWIReport',
-        geometry: { longitude: 'lon', latitude: 'lat' },
-        visualizations: {
-          radius: config?.mapValueKeys ?
-            {
-              value: {
-                field: `${getKeyValues(mapValueKeys, mapVis.RADIUS)?.key}_${getKeyValues(mapValueKeys, mapVis.RADIUS)?.agg}`,
-              },
-              valueOptions: [5, 15],
-              dataScale: 'linear',
-            }:
-            null,
-          fill: getKeyValues(config?.mapValueKeys, mapVis.FILL)?.key ?
-            {
-              value: {
-                field: `${getKeyValues(mapValueKeys, mapVis.FILL)?.key}_${getKeyValues(mapValueKeys, mapVis.FILL)?.agg}`,
-              },
-              valueOptions: [[214, 232, 253], [39, 85, 196]],
-              dataScale: 'linear',
-            }:
-            null,
-        },
+        geometry,
+        visualizations: Object.fromEntries(mapValueKeys.map(({ key, agg, map_vis }) => [
+          map_vis,
+          {
+            value: {
+              field: `${key}_${agg}`,
+            },
+            valueOptions: VIS_OPTIONS.values[map_vis],
+            dataScale: VIS_OPTIONS.scale,
+          },
+        ])),
         interactions: {
           tooltip: {
             tooltipKeys: {
-              name: 'poi_name',
+              name,
+              id,
             },
           },
         },
         legend: { showLegend: true },
-        opacity: 0.3,
+        //----TO DO - ERIKA - add to state for editor
+        opacity: OPACITY,
       }],
       mapConfig: {
         cursor: (layers) => getCursor({ layers }),
