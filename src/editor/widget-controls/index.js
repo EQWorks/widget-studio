64bf51edfa7a355b56e1c9797018170d61686c45
Kyle Grimsrud-Manz <hi@kylegrimsrudma.nz>
2021-11-07 11:17:52 -0600

    CustomAccordion - introduce CustomAccordion

--- src/editor/widget-controls/index.js
+++ src/editor/widget-controls/index.js
@@ -1,4 +1,4 @@
-import React, { useEffect, createElement } from 'react'
+import React, { createElement } from 'react'
 import PropTypes from 'prop-types'
 
 import { Icons as _Icons } from '@eqworks/lumen-labs'
@@ -11,8 +11,8 @@ import ScatterControls from './controls/scatter-controls'
 import { Controls } from '../../components/icons'
 import CustomButton from '../../components/custom-button'
 import Icons from './icons'
-import { useResizeDetector } from 'react-resize-detector'
 import WidgetControlCard from '../shared-components/widget-control-card'
+import CustomAccordion from '../../components/custom-accordion'
 
 
 const controls = {
@@ -22,77 +22,49 @@ const controls = {
   scatter: ScatterControls,
 }
 
-const WidgetControls = ({ className }) => {
-
+const WidgetControls = () => {
   const nestedUpdate = useStoreActions(actions => actions.nestedUpdate)
-  const update = useStoreActions(actions => actions.update)
   const resetWidget = useStoreActions(actions => actions.resetWidget)
   const type = useStoreState((state) => state.type)
   const columns = useStoreState((state) => state.columns)
-
-  const showWidgetControls = useStoreState((state) => state.ui.showWidgetControls)
   const dataReady = useStoreState((state) => state.dataReady)
-
-  const { height, ref } = useResizeDetector()
-
-  useEffect(() => {
-    update({ numericColumns: columns.filter(({ category }) => category === 'Numeric').map(({ name }) => name) })
-    update({ stringColumns: columns.filter(({ category }) => category === 'String').map(({ name }) => name) })
-  }, [columns, update])
+  const showWidgetControls = useStoreState((state) => state.ui.showWidgetControls)
 
   return (
-    <>
-      <div style={{ height }} className={`${className} relative z-10 border-l-2 border-neutral-100 overflow-hidden transition-width duration-200 ease-in ${showWidgetControls ? 'w-0 border-l-none' : 'w-16 delay-500'}`}>
-        <CustomButton
-          variant='borderless'
-          className={'w-full justify-center border-none h-full'}
-          onClick={() => nestedUpdate({ ui: { showWidgetControls: !showWidgetControls } })}
+    <CustomAccordion
+      icon={Controls}
+      title={'Controls'}
+      open={showWidgetControls}
+      toggle={() => nestedUpdate({ ui: { showWidgetControls: !showWidgetControls } })}
+    >
+      <div className='flex flex-col'>
+        <WidgetControlCard
+          title='Select Widget Type'
+          clearable
         >
-          <Controls size='md' className='h-full stroke-current text-secondary-500 w-full p-5' />
-        </CustomButton>
-      </div>
-      <div ref={ref} className={`${className} relative z-10 border-l-2 border-neutral-100 transition-max-width overflow-x-hidden ease-in-out flex justify-end duration-1000 ${showWidgetControls ? 'max-w-full' : 'max-w-0'}`}>
-        <div className={'overflow-x-hidden'} >
-          <div className={'px-4 py-3 border-b border-neutral-100 flex items-center'}>
-            <span className='flex-1 font-bold text-secondary-800 text-md' >Controls</span >
-            <CustomButton
-              variant='borderless'
-              className={`border-none ${showWidgetControls ? '' : 'h-full'}`}
-              onClick={() => nestedUpdate({ ui: { showWidgetControls: !showWidgetControls } })}
-            >
-              <_Icons.Close size='md' className='fill-current text-secondary-500 h-min w-auto' />
-            </CustomButton>
-          </div >
-          <div className='px-4 py-2'>
-            <WidgetControlCard
-              title='Select Widget Type'
-              clearable
-            >
-              <Icons disabled={!columns.length || !dataReady} />
-            </WidgetControlCard>
+          <Icons disabled={!columns.length || !dataReady} />
+        </WidgetControlCard>
 
+        {
+          dataReady &&
+          <>
             {
-              dataReady &&
-              <>
-                {
-                  type && columns &&
-                  createElement(controls[type])
-                }
-                <div className={'p-2 flex justify-end'}>
-                  <CustomButton
-                    variant='borderless'
-                    className='border-none'
-                    onClick={resetWidget}
-                  >
-                    <_Icons.Close size='md' className='fill-current text-secondary-500 h-min w-auto' />
-                  </CustomButton>
-                </div>
-              </>
+              type && columns &&
+              createElement(controls[type])
             }
-          </div >
-        </div >
+            <div className={'p-2 flex justify-end'}>
+              <CustomButton
+                variant='borderless'
+                className='border-none'
+                onClick={resetWidget}
+              >
+                <_Icons.Close size='md' className='fill-current text-secondary-500 h-min w-auto' />
+              </CustomButton>
+            </div>
+          </>
+        }
       </div>
-    </>
+    </CustomAccordion>
   )
 }
 
