bc6f96ae96ff1295491e6bf6a158153fb0a660eb
Kyle Grimsrud-Manz <hi@kylegrimsrudma.nz>
2021-11-15 22:48:59 -0500

    devops - simplify storybook template

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,20 +1,4 @@
-
-import React from "react"
-import { ThemeProvider } from '@eqworks/lumen-ui'
-
-import { Authenticated } from "@eqworks/common-login"
-
 export const parameters = {
-  actions: { argTypesRegex: "^on[A-Z].*" },
+  controls: { disabled: true },
   layout: 'fullscreen',
 }
-
-export const decorators = [
-  (Story) => (
-    // <Authenticated product='locus'>
-      <ThemeProvider>
-        <Story />
-      </ThemeProvider>
-    // </Authenticated>
-  )
-]
