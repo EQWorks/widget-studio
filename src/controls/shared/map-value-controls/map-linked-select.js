6d60526deca70bcf6af64a116e1abae0a3401e0b
Erika Szabo <erika.szabo.geo@gmail.com>
2022-01-07 19:48:43 -0500

    MapLinkedSelect - transition to new design for map key controls

--- src/controls/shared/map-value-controls/map-linked-select.js
+++ src/controls/shared/map-value-controls/map-linked-select.js
@@ -1,11 +1,16 @@
 import React, { useMemo } from 'react'
 import PropTypes from 'prop-types'
 
-import Divider from '@material-ui/core/Divider'
-import { Typography } from '@eqworks/lumen-ui'
+import { Chip, makeStyles } from '@eqworks/lumen-labs'
 
 import LinkedSelect from '../../../components/linked-select'
+import { getLongest } from '../../../util'
 
+
+const classes = makeStyles({
+  keyControl1 : { marginTop: '15px' },
+  linkedSelect : { marginTop: '5px' },
+})
 
 const MapLinkedSelect = ({
   categories,
@@ -25,36 +30,36 @@ const MapLinkedSelect = ({
     categories.map((mapVis, i) => {
       const match = values.findIndex(v => v.mapVis === mapVis)
       return (
-        <div key={i}>
-          <div>
-            <Typography
-              color='textSecondary'
-              variant='body1'
-            >
-              {mapVis.charAt(0).toUpperCase() + mapVis.slice(1)}
-            </Typography>
-            <Typography
-              color='textSecondary'
-              variant='body1'
-            >
-              {'Aggregation'}
-            </Typography>
+        <div key={i} className={i === 0 ? '' : classes.keyControl1} >
+          <div className='invisible h-0 grid grid-cols-min-min pointer-events-none'>
+            <LinkedSelect
+              data={[]}
+              subData={[]}
+              deletable
+              callback={() => {}}
+              placeholders={[getLongest([titles[0], ...data]), getLongest([titles[1], ...subData])]}
+            />
           </div>
-          <Divider />
-          <LinkedSelect
-            key={mapVis}
-            callback={([_k, _v]) => callback(match, { key: _k, agg: _v, mapVis })}
-            data={remainingValues}
-            subData={subData}
-            deletable={false}
-            clearable
-            deleteCallback={() => deleteCallback(match)}
-            init={values[match]?.key}
-            subInit={values[match]?.agg}
-            placeholders={titles}
-            disableSub={disableSubFor.includes(values[match]?.key)}
-            disableSubMessage={`${values[match]?.key} ${disableSubMessage}`}
-          />
+          <Chip color={i === 0 ? 'primary' : 'success'}>
+            {mapVis.toUpperCase()}
+          </Chip>
+          <div className='grid grid-cols-min-1fr'>
+            <LinkedSelect
+              key={mapVis}
+              className={classes.linkedSelect}
+              callback={([_k, _v]) => callback(match, { key: _k, agg: _v, mapVis })}
+              data={remainingValues}
+              subData={subData}
+              deletable={false}
+              clearable
+              deleteCallback={() => deleteCallback(match)}
+              init={values[match]?.key}
+              subInit={values[match]?.agg}
+              placeholders={titles}
+              disableSub={disableSubFor.includes(values[match]?.key)}
+              disableSubMessage={`${values[match]?.key} ${disableSubMessage}`}
+            />
+          </div>
         </div>
       )
     })
