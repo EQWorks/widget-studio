707e2f543f272b4a6a84fde2d3e3e9f4a515d382
Erika Szabo <erika.szabo.geo@gmail.com>
2022-01-14 13:01:05 -0500

    util - add geoKeyHasCoordinates function

--- src/util/index.js
+++ src/util/index.js
@@ -1,1 +1,10 @@
-export const getLongestString = (arr) => arr.reduce((a, b) => (a.length > b.length ? a : b))
+import { MAP_LAYER_GEO_KEYS, COORD_KEYS } from '../constants/map'
+
+
+export const getLongestString = arr => arr.reduce((a, b) => (a.length > b.length ? a : b))
+
+// TO DO: extend conditions for polygon geojson & arc layers when implementing those layers as well
+export const geoKeyHasCoordinates = (geoKey, numericColumns) =>
+  (MAP_LAYER_GEO_KEYS.scatterplot.includes(geoKey) &&
+    numericColumns?.some(key => COORD_KEYS.latitude.includes(key)) &&
+    numericColumns?.some(key => COORD_KEYS.longitude.includes(key)))
