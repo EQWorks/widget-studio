0c210f22809c338b99592828b1c712d71ec150df
Erika Szabo <erika.szabo.geo@gmail.com>
2022-01-27 20:00:04 -0500

    util - remove getLongestString

--- src/util/index.js
+++ src/util/index.js
@@ -1,8 +1,6 @@
 import { MAP_LAYER_GEO_KEYS, COORD_KEYS } from '../constants/map'
 
 
-export const getLongestString = arr => arr.reduce((a, b) => (a.length > b.length ? a : b))
-
 // TO DO: extend conditions for polygon geojson & arc layers when implementing those layers as well
 export const geoKeyHasCoordinates = (geoKey, numericColumns) =>
   (MAP_LAYER_GEO_KEYS.scatterplot.includes(geoKey) &&
