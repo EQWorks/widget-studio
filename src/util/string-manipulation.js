e7122630e6f7a5b71ae92b9a9adb8741d85d6441
Erika Szabo <erika.szabo.geo@gmail.com>
2022-01-27 19:59:19 -0500

    util/string-manipulation - add getLongestString

--- src/util/string-manipulation.js
+++ src/util/string-manipulation.js
@@ -2,3 +2,4 @@ const capitalize = s => s[0].toUpperCase() + s.slice(1)
 const replaceUnderscore = s => s.replace(/_/g, ' ')
 
 export const cleanUp = s => capitalize(replaceUnderscore(s))
+export const getLongestString = arr => arr.reduce((a, b) => (a.length > b.length ? a : b))
