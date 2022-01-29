864bfda5491129e7019f785387579e30beaf910b
Kyle Grimsrud-Manz <hi@kylegrimsrudma.nz>
2022-01-29 15:57:38 -0500

    WidgetControlCard - dynamic styling from mode + refactor

--- src/controls/shared/widget-control-card.js
+++ src/controls/shared/widget-control-card.js
@@ -2,62 +2,135 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import clsx from 'clsx'
 
+import { getTailwindConfigColor, makeStyles } from '@eqworks/lumen-labs'
+
 import CustomButton from '../../components/custom-button'
 import { Trash } from '../../components/icons'
+import { useStoreState } from '../../store'
+import modes from '../../constants/modes'
 
 
-const WidgetControlCard = ({ title, titleExtra, description, clearable, showIfEmpty, children, className }) => {
+const commonClasses = {
+  titleText: {
+    flex: 1,
+  },
+  clearButtonInternalContainer: {
+    display: 'flex',
+    alignItems: 'center',
+  },
+}
+
+const useStyles = (mode = modes.EDITOR) => makeStyles(
+  mode === modes.EDITOR
+    ? {
+      outerContainer: {
+        padding: '0.8rem',
+        borderBottom: `1px solid ${getTailwindConfigColor('neutral-100')}`,
+      },
+      titleContainer: {
+        display: 'flex',
+        fontSize: '0.875rem',
+        fontWeight: 700,
+        color: getTailwindConfigColor('secondary-900'),
+        marginBottom: '0.7rem',
+        padding: '0 0.5rem',
+      },
+      clearButton: {
+        textTransform: 'capitalize',
+        fontWeight: 600,
+        fontSize: '0.75rem',
+        color: getTailwindConfigColor('secondary-400'),
+      },
+      content: {
+        padding: '0 0.5rem',
+      },
+      ...commonClasses,
+    }
+    : {
+      outerContainer: {
+        borderRadius: '0.25rem',
+        margin: '0.25rem 0',
+        border: `1px solid ${getTailwindConfigColor('neutral-100')}`,
+      },
+      titleContainer: {
+        display: 'flex',
+        fontSize: '0.875rem',
+        fontWeight: 600,
+        color: getTailwindConfigColor('secondary-700'),
+        background: getTailwindConfigColor('neutral-100'),
+        padding: '0.4rem 0.75rem',
+      },
+      clearButton: {
+        display: 'flex',
+        padding: '0.125rem 0.375rem',
+        transitionProperty: 'all 0.3s ease',
+        fontWeight: 500,
+        letterSpacing: '0.05em',
+        textTransform: 'uppercase',
+        alignItems: 'center',
+        fontSize: '0.65rem',
+        borderRadius: '0.125rem',
+        transition: 'all 0.3s ease',
+        color: getTailwindConfigColor('secondary-600'),
+        '& svg': {
+          transition: 'all 0.3s ease',
+          fill: getTailwindConfigColor('secondary-600'),
+          marginLeft: '0.2rem',
+        },
+        '&:hover': {
+          color: getTailwindConfigColor('secondary-800'),
+          '& svg': {
+            fill: getTailwindConfigColor('secondary-800'),
+          },
+        },
+        background: getTailwindConfigColor('secondary-50'),
+      },
+      content: {
+        padding: '0.5rem 0.75rem',
+      },
+      description: {
+        color: getTailwindConfigColor('secondary-600'),
+        paddingLeft: '0.75rem',
+        paddingRight: '0.75rem',
+        paddingBottom: '0.25rem',
+        paddingTop: '0.5rem',
+        fontSize: '0.75rem',
+        lineHeight: '1rem',
+        fontStyle: 'italic',
+        letterSpacing: '0.025em',
+      },
+      ...commonClasses,
+    }
+)
+
+const WidgetControlCard = ({ title, titleExtra, description, clearable, children }) => {
+  const mode = useStoreState((state) => state.ui.mode)
+  const classes = useStyles(mode)
+
+  const renderTitle = (
+    <div className={classes.titleContainer}>
+      <div className={classes.titleText}>
+        {`${title}:`}
+      </div>
+      {titleExtra}
+      {clearable &&
+        <CustomButton
+          className={clsx(classes.clearButton, { 'shadow-light-10 hover:shadow-light-20': mode === modes.QL })}
+          onClick={() => alert('not implemented')}
+        >
+          <div className={classes.clearButtonInternalContainer}>
+            clear {mode === modes.QL && <Trash size='md' />}
+          </div>
+        </CustomButton>}
+    </div >
+  )
+
   return (
-    (children || showIfEmpty) &&
-    <div className={`rounded-sm my-1 border border-neutral-100 ${className}`}>
-      {
-        title &&
-        <div className='p-2 py-1.5 bg-neutral-100 text-secondary-700 font-semibold text-sm flex align-center'>
-          <div className='flex-1'>
-            {`${title}:`}
-          </div>
-          {titleExtra}
-          {
-            clearable &&
-            <CustomButton
-              onClick={() => alert('not implemented')}
-              className={clsx(
-                'flex flex-row items-center font-medium rounded-sm tracking-wider uppercase px-1.5 py-0.5 transition-all ease-in-out duration-300',
-                'text-xs text-secondary-600 hover:text-secondary-800',
-                'bg-secondary-50',
-                'shadow-light-10 hover:shadow-light-20',
-              )}
-            >
-              <div className='flex items-center'>
-                <span className='mr-1'>clear</span>
-                <Trash size='md' className='fill-current text-secondary-600 hover:text-secondary-800' />
-              </div>
-            </CustomButton>
-          }
-        </div>
-      }
-      {
-        children &&
-        (description
-          ? <>
-            <div className='table w-full'>
-              <div className='table-cell w-0'>
-                <div className='px-3 pt-2 pb-1 break-word italic text-xs text-secondary-600 tracking-wide'>
-                  {description}
-                </div>
-              </div>
-              <div className='table-row w-full'>
-                <div className='w-full h-full flex flex-col px-3 py-2'>
-                  {children}
-                </div>
-              </div>
-            </div>
-          </>
-          : <div className='flex flex-col px-3 py-2'>
-            {children}
-          </div>
-        )
-      }
+    children &&
+    <div className={classes.outerContainer}>
+      {title && renderTitle}
+      {description && <div className={classes.description}> {description} </div>}
+      <div className={classes.content}> {children} </div>
     </div>
   )
 }
@@ -68,10 +141,6 @@ WidgetControlCard.propTypes = {
   titleExtra: PropTypes.node,
   description: PropTypes.node,
   clearable: PropTypes.bool,
-  showIfEmpty: PropTypes.bool,
-  grow: PropTypes.bool,
-  ignore: PropTypes.bool,
-  className: PropTypes.string,
 }
 
 WidgetControlCard.defaultProps = {
@@ -80,10 +149,6 @@ WidgetControlCard.defaultProps = {
   titleExtra: null,
   description: null,
   clearable: false,
-  showIfEmpty: false,
-  grow: false,
-  ignore: false,
-  className: '',
 }
 
 export default WidgetControlCard
