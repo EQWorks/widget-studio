e38db5e6ec560c5b1170846e87cbce17b98f5223
Erika Szabo <erika.szabo.geo@gmail.com>
2022-01-31 20:00:18 -0500

    adapters/react-maps - add formatData prop to layerConfig obj

--- src/view/adapter/adapters/react-maps.js
+++ src/view/adapter/adapters/react-maps.js
@@ -125,6 +125,7 @@ export default {
             },
           ]
         })),
+        formatData: config.formatDataFunctions,
         interactions: {
           tooltip: {
             tooltipKeys: {
