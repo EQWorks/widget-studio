340b45276fa9106362a510ee9c6b4c914159ba2f
Erika Szabo <erika.szabo.geo@gmail.com>
2022-02-10 13:30:19 -0500

    EditorMode/RightSidebar - deleted StylingMap comp and integrate all map controls in the same file as for charts; change section order in stayling and add titles for chart sections

--- src/controls/editor-mode/right-sidebar.js
+++ src/controls/editor-mode/right-sidebar.js
@@ -1,9 +1,8 @@
 import React from 'react'
 
-import { makeStyles } from '@eqworks/lumen-labs'
+import { makeStyles, getTailwindConfigColor } from '@eqworks/lumen-labs'
 
 import { useStoreState, useStoreActions } from '../../store'
-import StylingMap from './styling-map'
 import WidgetControlCard from '../shared/components/widget-control-card'
 import { positions, sizes } from '../../constants/viz-options'
 import types from '../../constants/types'
@@ -11,19 +10,25 @@ import CustomSelect from '../../components/custom-select'
 import XYSelect from '../../components/xy-select'
 import ColorSchemeControls from './components/color-scheme-controls'
 import { renderItem, renderSection, renderRow, renderToggle } from '../shared/util'
-import UniqueOptionControls from './components/unique-option-controls'
 import EditorSidebarBase from './sidebar-base'
 import Filters from './filters'
 import CustomDropdown from './components/custom-dropdown'
 import MutedBarrier from '../shared/muted-barrier'
+import { MAP_LEGEND_SIZE } from '../../constants/map'
 
 
 const classes = makeStyles({
+  displayOptions: {
+    marginTop: '0.625rem',
+  },
   xyDropdownMenu: {
     height: '5rem',
     display: 'flex',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  legendSize: {
+    color: getTailwindConfigColor('primary-600'),
   },
 })
 
@@ -38,99 +43,117 @@ const EditorRightSidebar = () => {
   const size = useStoreState((state) => state.genericOptions.size)
   const titlePosition = useStoreState((state) => state.genericOptions.titlePosition)
   const legendPosition = useStoreState((state) => state.genericOptions.legendPosition)
+  const legendSize = useStoreState((state) => state.genericOptions.legendSize)
   const showLegend = useStoreState((state) => state.genericOptions.showLegend)
+  const showTooltip = useStoreState((state) => state.genericOptions.showTooltip)
 
   return (
     <EditorSidebarBase>
       <MutedBarrier mute={!type || !domain?.value || !(renderableValueKeys.length)} >
         <Filters />
-        <WidgetControlCard title='Styling'>
-          {type !== types.MAP &&
-            renderSection(null,
+        <WidgetControlCard title={type === types.MAP ? 'Map Settings' : 'Chart Settings'}>
+          <div className={classes.displayOptions}>
+            {renderSection(
+              'Display Options',
               <>
-                {renderRow(null,
-                  <>
-                    {renderItem('Title Position',
-                      <CustomDropdown
-                        selectedString={positions.string[positions.numeric.map(JSON.stringify).indexOf(JSON.stringify(titlePosition))]}
-                        classes={{ menu: classes.xyDropdownMenu }}
-                        disabled={!showWidgetTitle && !subPlots}
-                      >
-                        <XYSelect
-                          value={titlePosition}
-                          disabled={[[0.5, 0.5], [0, 0.5], [1, 0.5]]}
-                          update={titlePosition => userUpdate({ genericOptions: { titlePosition } })}
-                        />
-                      </CustomDropdown>
-                    )}
-                    {renderItem('Legend Position',
-                      <CustomDropdown
-                        disabled={!showLegend}
-                        selectedString={positions.string[positions.numeric.map(JSON.stringify).indexOf(JSON.stringify(legendPosition))]}
-                        classes={{ menu: classes.xyDropdownMenu }}
-                      >
-                        <XYSelect
-                          value={legendPosition}
-                          update={legendPosition => userUpdate({ genericOptions: { legendPosition } })}
-                        />
-                      </CustomDropdown>
-                    )}
-                  </>
+                {renderToggle(
+                  'Legend',
+                  showLegend,
+                  v => userUpdate({ genericOptions: { showLegend: v } }),
                 )}
-                {subPlots && renderRow('Subplot Size',
-                  <CustomSelect
-                    fullWidth
-                    data={sizes.string}
-                    value={sizes.string[sizes.numeric.indexOf(size)]}
-                    onSelect={v => userUpdate({ genericOptions: { size: sizes.dict[v] } })}
-                  />
-                )}
+                {type === types.MAP &&
+                  renderToggle(
+                    'Tooltip',
+                    showTooltip,
+                    v => userUpdate({ genericOptions: { showTooltip: v } }),
+                  )
+                }
+                {type !== types.MAP && type !== types.PIE &&
+                  renderToggle(
+                    'Subplots',
+                    subPlots,
+                    v => userUpdate({ genericOptions: { subPlots: v } }),
+                    valueKeys.length <= 1
+                  )}
+                {type !== types.MAP &&
+                  renderToggle(
+                    'Widget Title',
+                    showWidgetTitle,
+                    v => userUpdate({ genericOptions: { showWidgetTitle: v } }),
+                  )
+                }
               </>
-            )
-          }
-          {renderSection(
-            'Display Options',
+            )}
+          </div>
+          {renderSection('Styling',
             <>
-              {type === types.MAP && <StylingMap />}
-              {type !== types.MAP &&
+              {renderRow(null,
                 <>
-                  <WidgetControlCard title='Styling'>
-                    {renderSection(null,
-                      <>
-                        {renderRow(null,
-                          <>
-                            {renderToggle(
-                              'Legend',
-                              showLegend,
-                              v => userUpdate({ genericOptions: { showLegend: v } }),
-                            )}
-                            {type !== types.PIE && type !== types.MAP &&
-                              renderToggle(
-                                'Subplots',
-                                subPlots,
-                                v => userUpdate({ genericOptions: { subPlots: v } }),
-                                valueKeys.length <= 1
-                              )}
-                            {type !== types.MAP &&
-                              renderToggle(
-                                'Widget Title',
-                                showWidgetTitle,
-                                v => userUpdate({ genericOptions: { showWidgetTitle: v } }),
-                              )}
-                          </>
-                        )}
-                        <UniqueOptionControls type={type} />
-                      </>
-                    )}
-                  </WidgetControlCard >
-                  <WidgetControlCard title='Colour Scheme'>
-                    <ColorSchemeControls />
-                  </WidgetControlCard >
+                  {type !== types.MAP && renderItem('Title Position',
+                    <CustomDropdown
+                      selectedString={positions.string[positions.numeric.map(JSON.stringify)
+                        .indexOf(JSON.stringify(titlePosition))]}
+                      classes={{ menu: classes.xyDropdownMenu }}
+                      disabled={!showWidgetTitle && !subPlots}
+                    >
+                      <XYSelect
+                        value={titlePosition}
+                        disabled={[[0.5, 0.5], [0, 0.5], [1, 0.5]]}
+                        update={titlePosition => userUpdate({ genericOptions: { titlePosition } })}
+                      />
+                    </CustomDropdown>
+                  )}
+                  {renderItem('Legend Position',
+                    <CustomDropdown
+                      disabled={!showLegend}
+                      selectedString={positions.string[positions.numeric.map(JSON.stringify)
+                        .indexOf(JSON.stringify(legendPosition))]}
+                      classes={{ menu: classes.xyDropdownMenu }}
+                    >
+                      <XYSelect
+                        value={legendPosition}
+                        update={legendPosition => userUpdate({ genericOptions: { legendPosition } })}
+                        disabled={type === types.MAP ?
+                          [[0.5, 0.5], [0, 0.5], [1, 0.5], [0.5, 0], [0.5, 1]] :
+                          [[0.5, 0.5]]}
+                      />
+                    </CustomDropdown>
+                  )}
+                  {type === types.MAP && renderItem('Legend Size',
+                    <CustomSelect
+                      allowClear={false}
+                      disabled={!showLegend}
+                      fullWidth
+                      data={Object.keys(MAP_LEGEND_SIZE)}
+                      value={legendSize}
+                      onSelect={legendSize => userUpdate({ genericOptions: { legendSize } })}
+                      placeholder={!showLegend ? 'N/A' : 'Select'}
+                      renderSelectedOptions={() =>
+                        showLegend &&
+                        <span className={classes.legendSize}>
+                          {legendSize}
+                        </span>
+                      }
+                    />
+                  )}
                 </>
-              }
+              )}
+              {type !== types.MAP && subPlots && renderRow('Subplot Size',
+                <CustomSelect
+                  fullWidth
+                  data={sizes.string}
+                  value={sizes.string[sizes.numeric.indexOf(size)]}
+                  onSelect={v => userUpdate({ genericOptions: { size: sizes.dict[v] } })}
+                />
+              )}
             </>
           )}
         </WidgetControlCard >
+        {type !== types.MAP &&
+          <WidgetControlCard title='Colour Scheme'>
+            <ColorSchemeControls />
+          </WidgetControlCard >
+        }
       </MutedBarrier>
     </EditorSidebarBase >
   )
