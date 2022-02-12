1d0d7ca6e8ad1a115752530a62912fa0ec1d9d18
Erika Szabo <erika.szabo.geo@gmail.com>
2022-02-12 16:59:50 -0500

    ColorSchemeControl - readd marginBottom to row styling mistakenly removed

--- src/controls/editor-mode/components/color-scheme-controls.js
+++ src/controls/editor-mode/components/color-scheme-controls.js
@@ -24,6 +24,7 @@ const useStyles = ({ baseColor, showPicker, type }) => makeStyles({
   row: {
     display: 'flex',
     justifyContent: 'space-between',
+    marginBottom: '0.6rem',
     alignItems: 'center',
   },
   swatchContainer: {
