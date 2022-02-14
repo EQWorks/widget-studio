b06f3dfca553c55f8df65b912cef8fb9edb89f68
Kyle Grimsrud-Manz <hi@kylegrimsrudma.nz>
2022-02-14 18:12:31 -0500

    constants - introduce columnTypes, columnTypeInfo

---
+++ src/constants/columns.js
@@ -0,0 +1,49 @@
+import { Icons } from '@eqworks/lumen-labs'
+import { priceStringToNumeric } from '../util/numeric'
+import { isString } from '../util/string-manipulation'
+
+
+export const columnTypes = {
+  NUMERIC: 'Numeric',
+  DATE: 'Date',
+  STRING: 'String',
+  PRICE: 'Price',
+}
+
+export const columnTypeInfo = {
+  // 'primitives':
+  [columnTypes.NUMERIC]: {
+    Icon: Icons.Hash,
+    validate: (v, name) => {
+      const res = !isNaN(v)
+      return name === undefined
+        ? res
+        : res && !name.endsWith('_id')
+    },
+  },
+  [columnTypes.STRING]: {
+    Icon: Icons.Edit,
+    validate: isString,
+  },
+  // subtypes:
+  [columnTypes.PRICE]: {
+    parentTypes: [
+      columnTypes.NUMERIC,
+    ],
+    Icon: Icons.Dollar,
+    validate: (v) => isString(v) && !isNaN(priceStringToNumeric(v)),
+  },
+  [columnTypes.DATE]: {
+    parentTypes: [
+      columnTypes.STRING,
+    ],
+    Icon: Icons.Table, // looks like a calendar
+    validate: (v) => {
+      if (!isString(v)) {
+        return false
+      }
+      const sample = new Date(v)
+      return sample instanceof Date && !isNaN(sample)
+    },
+  },
+}
