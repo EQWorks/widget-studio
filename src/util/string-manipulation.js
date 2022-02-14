e82add53e8154771727f03676194200b955a81c4
Kyle Grimsrud-Manz <hi@kylegrimsrudma.nz>
2022-02-14 18:12:31 -0500

    constants - introduce columnTypes, columnTypeInfo

--- src/util/string-manipulation.js
+++ src/util/string-manipulation.js
@@ -1,2 +1,3 @@
 export const cleanUp = s => s.replace(/_/g, ' ').replace(/./, v => v.toUpperCase())
 export const getLongestString = arr => arr.reduce((a, b) => (a.length > b.length ? a : b))
+export const isString = v => typeof v === 'string' || v instanceof String
