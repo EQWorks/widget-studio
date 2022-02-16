34e52fc21f3b126648c419d75ae0c925db365948
Kyle Grimsrud-Manz <hi@kylegrimsrudma.nz>
2022-02-16 03:33:48 -0500

    constants - introduce export constants

---
+++ src/constants/export.js
@@ -0,0 +1,12 @@
+export const EXPORT_TYPES = [
+  {
+    label: 'PNG',
+    mime: 'image/png',
+    extension: 'png',
+  },
+  {
+    label: 'JPEG',
+    mime: 'image/jpeg',
+    extension: 'jpeg',
+  },
+]
