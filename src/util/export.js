c1275195b4bb133727be163057f633492962f7e8
Kyle Grimsrud-Manz <hi@kylegrimsrudma.nz>
2022-02-16 03:32:57 -0500

    util - introduce export/screenshot

---
+++ src/util/export.js
@@ -0,0 +1,23 @@
+import html2canvas from 'html2canvas'
+
+
+export const screenshot = (node, type) => (
+  node && html2canvas(node, { logging: false })
+    .then((canvas) => {
+      const croppedCanvas = document.createElement('canvas')
+      const croppedCanvasContext = croppedCanvas.getContext('2d')
+      const cropPositionTop = 0
+      const cropPositionLeft = 0
+      const cropWidth = canvas.width
+      const cropHeight = canvas.height
+      croppedCanvas.width = cropWidth
+      croppedCanvas.height = cropHeight
+      croppedCanvasContext.drawImage(
+        canvas,
+        cropPositionLeft,
+        cropPositionTop,
+      )
+      const base64Image = croppedCanvas.toDataURL(type, 1.0)
+      return base64Image
+    })
+)
