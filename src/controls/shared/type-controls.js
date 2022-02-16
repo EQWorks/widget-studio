247c3133c69f5eb63298adf459aa3093b9281fda
Kyle Grimsrud-Manz <hi@kylegrimsrudma.nz>
2022-02-16 04:56:21 -0500

    controls - rework WidgetTypeControls

--- src/controls/shared/type-controls.js
+++ src/controls/shared/type-controls.js
@@ -1,76 +1,236 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 
-import clsx from 'clsx'
+import { getTailwindConfigColor, makeStyles, Tooltip } from '@eqworks/lumen-labs'
 
-import CustomButton from '../../components/custom-button'
 import { useStoreActions, useStoreState } from '../../store'
 import types from '../../constants/types'
 import typeInfo from '../../constants/type-info'
+import modes from '../../constants/modes'
+import WidgetControlCard from './components/widget-control-card'
 
 
-const Icons = ({ disabled }) => {
+const commonClasses = {
+  titleText: {
+    flex: 1,
+  },
+  description: {
+    color: getTailwindConfigColor('secondary-600'),
+    paddingLeft: '0.75rem',
+    paddingRight: '0.75rem',
+    paddingBottom: '0.25rem',
+    paddingTop: '0.5rem',
+    fontSize: '0.75rem',
+    lineHeight: '1rem',
+    fontStyle: 'italic',
+    letterSpacing: '0.025em',
+  },
+  button: {
+    outline: 'none !important',
+    '&:hover': {
+      outline: 'none !important',
+    },
+    '&:active': {
+      outline: 'none !important',
+    },
+  },
+  disabledButton: {
+    cursor: 'default !important',
+  },
+  disabledIcon: {
+    '&> p': {
+      color: `${getTailwindConfigColor('secondary-400')} !important`,
+    },
+    '&> svg': {
+      background: `${getTailwindConfigColor('secondary-100')} !important`,
+      color: `${getTailwindConfigColor('secondary-400')} !important`,
+    },
+    '&:hover': {
+      '&> svg': {
+        background: `${getTailwindConfigColor('secondary-100')} !important`,
+        color: `${getTailwindConfigColor('secondary-400')} !important`,
+      },
+      '&> p': {
+        color: `${getTailwindConfigColor('secondary-400')} !important`,
+      },
+    },
+  },
+  selectedIcon: {
+    '&> p': {
+      color: `${getTailwindConfigColor('primary-600')} !important`,
+    },
+    '&> svg': {
+      background: `${getTailwindConfigColor('primary-200')} !important`,
+      color: `${getTailwindConfigColor('primary-700')} !important`,
+    },
+    '&:hover': {
+      '&> svg': {
+        background: `${getTailwindConfigColor('primary-200')} !important`,
+        color: `${getTailwindConfigColor('primary-700')} !important`,
+      },
+    },
+  },
+}
+
+const baseIconStyle = {
+  display: 'flex',
+  alignItems: 'center',
+  justifyContent: 'center',
+  width: '1.857rem',
+  '&> p': {
+    transition: 'color 0.3s, background 0.3s',
+    color: getTailwindConfigColor('secondary-600'),
+  },
+  '&> svg': {
+    transition: 'color 0.3s, background 0.3s',
+    width: '100%',
+    borderRadius: '0.286rem',
+    padding: '0.387rem',
+    color: getTailwindConfigColor('primary-500'),
+    background: getTailwindConfigColor('primary-50'),
+  },
+  '&:hover': {
+    '&> svg': {
+      background: getTailwindConfigColor('primary-100'),
+      color: getTailwindConfigColor('primary-600'),
+    },
+    '&> p': {
+      color: getTailwindConfigColor('secondary-700'),
+    },
+  },
+  '&:active': {
+    '&> svg': {
+      color: getTailwindConfigColor('primary-700'),
+    },
+  },
+}
+
+const useStyles = ({ mode = modes.EDITOR }) => makeStyles(
+  mode === modes.EDITOR
+    ? {
+      outerContainer: {
+        display: 'flex',
+        justifyContent: 'space-between',
+      },
+      icon: {
+        ...baseIconStyle,
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+      },
+      label: {
+        textTransform: 'capitalize',
+        fontWeight: 700,
+        fontSize: '0.714rem',
+        marginTop: '0.4rem',
+      },
+      ...commonClasses,
+    }
+    : {
+      outerContainer: {
+        display: 'flex',
+      },
+      icon: {
+        ...baseIconStyle,
+        '--tw-shadow': '0px 1px 4px rgba(54, 111, 228, 0.1)',
+        boxShadow: 'var(--tw-ring-offset-shadow, 0 0 #0000), var(--tw-ring-shadow, 0 0 #0000), var(--tw-shadow)',
+        border: `0.3rem solid ${getTailwindConfigColor('secondary-50')}`,
+        width: '2.214rem',
+        borderRadius: '0.5rem',
+        marginRight: '0.8rem',
+        '&> svg': {
+          ...baseIconStyle['&> svg'],
+          padding: '0.3rem',
+        },
+      },
+      label: {
+        display: 'none',
+
+      },
+      ...commonClasses,
+    }
+)
+
+
+const WidgetTypeControls = () => {
   const userUpdate = useStoreActions((actions) => actions.userUpdate)
-  const current = useStoreState((state) => state.type)
+  const selectedType = useStoreState((state) => state.type)
   const group = useStoreState((state) => state.group)
   const domain = useStoreState((state) => state.domain)
   const validMapGroupKeys = useStoreState((state) => state.validMapGroupKeys)
+  const mode = useStoreState((state) => state.ui.mode)
 
-  const mapIconAvailability = validMapGroupKeys.length > 0
+  const classes = useStyles({ mode })
 
-  const iconButtonClass = (isCurrent, isDisabled) => clsx('outline-none focus:outline-none border-white border-custom-1 shadow-light-10 hover:shadow-light-20 h-10 w-10 p-1 flex items-center justify-center mr-3 rounded-xl transition-all duration-300 ease-in-out', {
-    ['text-primary-500 hover:text-primary-600 active:text-primary-700 bg-primary-50 hover:bg-primary-100']: !isCurrent && !isDisabled,
-    ['text-primary-700 hover:text-primary-700 bg-primary-200 hover:bg-primary-200']: isCurrent,
-    ['pointer-events-none text-secondary-400 bg-secondary-100']: isDisabled,
-  })
   return (
-    <div className='flex'>
-      {
-        Object.entries(typeInfo).map(([type, { groupingOptional, icon: Icon, uniqueOptions }], i) => {
-          const isCurrent = type === current
-          const isDisabled = disabled || (type === types.MAP && !mapIconAvailability)
-          return (
-            <CustomButton
-              key={i}
-              disabled={isDisabled}
-              variant='borderless'
-              className={iconButtonClass(isCurrent, isDisabled)}
-              onClick={() => {
-                const willGroup = !groupingOptional
-                userUpdate({
-                  group: willGroup,
-                  ...(
-                    willGroup !== group && {
-                      groupFilter: [],
-                      valueKeys: [],
-                      ...(domain?.key && { [domain.key]: null }),
-                    }
-                  ),
-                  type,
-                  uniqueOptions:
-                    Object.entries(uniqueOptions).reduce((acc, [k, { defaultValue }]) => {
-                      acc[k] = defaultValue
-                      return acc
-                    }, {}),
+    <WidgetControlCard title='Widget Type'>
+      <div className={classes.outerContainer}>
+        {
+          Object.entries(typeInfo).map(([type, { groupingOptional, icon: Icon, uniqueOptions }], i) => {
+            const disabled = type === types.MAP && !(validMapGroupKeys?.length)
+            let styles = [classes.icon]
+            disabled && styles.unshift(classes.disabledIcon)
+            type === selectedType && styles.unshift(classes.selectedIcon)
+            const renderIcon = (
+              <div className={styles.join(' ')}>
+                <Icon />
+                <p className={classes.label}>
+                  {type}
+                </p>
+              </div>
+            )
+            return (
+              <button
+                key={i}
+                variant='borderless'
+                className={`${classes.button} ${disabled ? classes.disabledButton : ''}`}
+                onClick={() => {
+                  if (!disabled && type !== selectedType) {
+                    const willGroup = !groupingOptional
+                    userUpdate({
+                      group: willGroup,
+                      ...(
+                        willGroup !== group && {
+                          groupFilter: [],
+                          valueKeys: [],
+                          ...(domain?.key && { [domain.key]: null }),
+                        }
+                      ),
+                      type,
+                      uniqueOptions:
+                        Object.entries(uniqueOptions).reduce((acc, [k, { defaultValue }]) => {
+                          acc[k] = defaultValue
+                          return acc
+                        }, {}),
+                    })
+                  }
+                }}
+              >
+                {
+                  disabled
+                    ? <Tooltip
+                      description='No geo columns found in this dataset'
+                      {...(mode === modes.EDITOR && { width: '10rem' })}
+                    >
+                      {renderIcon}
+                    </Tooltip>
+                    : renderIcon
                 }
-                )
-              }}
-            >
-              <Icon className='overflow-visible w-full h-full fill-current' />
-            </CustomButton>
+              </button>
+            )
+          }
           )
         }
-        )
-      }
-    </div >
+      </div >
+    </WidgetControlCard>
   )
 }
 
-Icons.propTypes = {
+WidgetTypeControls.propTypes = {
   disabled: PropTypes.bool,
 }
-Icons.defaultProps = {
+WidgetTypeControls.defaultProps = {
   disabled: false,
 }
 
-export default Icons
+export default WidgetTypeControls
