13a1b57363708e9cf0abbb5872f360a19408d5fa
Kyle Grimsrud-Manz <hi@kylegrimsrudma.nz>
2022-02-18 17:54:24 -0500

    controls - introduce temporary DataSourceControls

--- src/controls/editor-mode/left-sidebar.js
+++ src/controls/editor-mode/left-sidebar.js
@@ -9,12 +9,14 @@ import ValueControls from '../shared/value-controls'
 import MapValueControls from '../shared/map-value-controls'
 import EditorSidebarBase from './sidebar-base'
 import DataTransformationControls from '../shared/data-transformation-controls'
+import DataSourceControls from './components/data-source-controls'
 
 
 const EditorLeftSidebar = () => {
   const type = useStoreState((state) => state.type)
   return (
     <EditorSidebarBase>
+      <DataSourceControls />
       <WidgetTypeControls />
       {
         type && type === types.MAP
