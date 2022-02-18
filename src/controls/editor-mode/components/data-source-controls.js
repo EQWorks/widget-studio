6bed88821853593a7a8da20383b4eaf892bf0279
Kyle Grimsrud-Manz <hi@kylegrimsrudma.nz>
2022-02-18 17:54:24 -0500

    controls - introduce temporary DataSourceControls

---
+++ src/controls/editor-mode/components/data-source-controls.js
@@ -0,0 +1,106 @@
+import React, { createElement, useMemo } from 'react'
+
+import { getTailwindConfigColor, makeStyles } from '@eqworks/lumen-labs'
+
+import { useExecutions } from '../../../util/fetch'
+import { useStoreState, useStoreActions } from '../../../store'
+import CustomSelect from '../../../components/custom-select'
+import { renderRow } from '../../shared/util'
+import WidgetControlCard from '../../shared/components/widget-control-card'
+import { columnTypeInfo } from '../../../constants/columns'
+import { dataSourceTypes } from '../../../constants/data-source'
+
+
+const classes = makeStyles({
+  dataSourceLabelContainer: {
+    width: '100%',
+    color: getTailwindConfigColor('secondary-600'),
+    fontWeight: 700,
+    fontSize: '0.857rem',
+    padding: '0.429rem 0.714rem',
+    '--tw-shadow': '0px 1px 4px rgba(81, 113, 151, 0.1)',
+    boxShadow: 'var(--tw-ring-offset-shadow, 0 0 #0000), var(--tw-ring-shadow, 0 0 #0000), var(--tw-shadow)',
+  },
+  dropdownDescriptionContainer: {
+    display: 'flex',
+  },
+  dropdownDescriptionText: {
+    flex: 1,
+  },
+  dropdownDescriptionIcons: {
+    display: 'flex',
+    '> *': {
+      marginRight: '0.4rem',
+    },
+    '> :last-child': {
+      marginRight: '0px !important',
+    },
+  },
+})
+
+const DataSourceControls = () => {
+  const userUpdate = useStoreActions((actions) => actions.userUpdate)
+  const dataSourceType = useStoreState((state) => state.dataSource.type)
+  const dataSourceID = useStoreState((state) => state.dataSource.id)
+  const cu = useStoreState((state) => state.cu)
+  const dev = useStoreState((state) => state.dev)
+  const [executionsLoading, executionsList] = useExecutions()
+
+  const executions = useMemo(() => (
+    executionsList.filter(({ customerID }) => customerID == cu || dev)
+      .map(({ queryID, executionID, columns, views = [] }) => (
+        {
+          id: executionID,
+          queryID,
+          label: `${executionID} - ${views[0]?.name}`,
+          description: (
+            <span key={executionID} className={classes.dropdownDescriptionContainer}>
+              <span className={classes.dropdownDescriptionText}>
+                {`${columns?.length} column${columns?.length !== 1 ? 's' : ''}`}
+              </span>
+              <span className={classes.dropdownDescriptionIcons}>
+                {[... new Set(columns.map(({ category }) => category))]
+                  .sort()
+                  .map(c => createElement(columnTypeInfo[c]?.Icon, { size: 'sm' }))}
+              </span>
+            </span>
+          ),
+        }
+      ))
+  ), [cu, dev, executionsList])
+
+  const selected = useMemo(() => (
+    executions.find(
+      dataSourceType === dataSourceTypes.SAVED_QUERIES
+        ? ({ queryID }) => Number(queryID) === Number(dataSourceID)
+        : ({ id }) => Number(id) === Number(dataSourceID)
+    )
+  ), [dataSourceID, dataSourceType, executions])
+
+  return (
+    <WidgetControlCard title='Data Source' >
+      {
+        renderRow(
+          'Query Execution',
+          <CustomSelect
+            allowClear={false}
+            disabled={executionsLoading}
+            placeholder={executionsLoading ? 'Loading...' : 'Select Execution'}
+            fullWidth
+            value={selected?.label}
+            descriptions={executions.map(({ description }) => description)}
+            data={executions.map(({ label }) => label)}
+            onSelect={v => userUpdate({
+              dataSource: {
+                type: dataSourceTypes.EXECUTIONS,
+                id: v.split(' ')[0],
+              },
+            })}
+          />
+        )
+      }
+    </WidgetControlCard >
+  )
+}
+
+export default DataSourceControls
