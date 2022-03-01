d5624fa47f984773eb5bded4496468cdab980d36
Kyle Grimsrud-Manz <hi@kylegrimsrudma.nz>
2022-03-01 18:38:11 -0500

    store - only reload data if source changes

--- src/store/model.js
+++ src/store/model.js
@@ -401,12 +401,11 @@ export default {
       })
   }),
 
-  loadConfig: thunk(async (actions, payload) => {
+  loadConfig: thunk(async (actions, payload, { getState }) => {
     actions.update({
       ignoreUndo: true,
       ui: {
         showDataSourceControls: false,
-        dataSourceLoading: true,
       },
     })
     const { dataSource, ...config } = payload
@@ -422,7 +421,11 @@ export default {
     })
     // populate state with values from config
     actions.update(config)
-    actions.loadData(dataSource)
+    const { dataSource: previousDataSource } = getState()
+    if (dataSource?.type !== previousDataSource?.type
+      && dataSource?.id !== previousDataSource?.id) {
+      actions.loadData(dataSource)
+    }
   }),
 
   loadData: thunk(async (actions, dataSource, { getState }) => {
