1d1594843020555346c2f7330b9c89650de64e3a
Kyle Grimsrud-Manz <hi@kylegrimsrudma.nz>
2022-03-16 11:35:24 -0400

    ui - table expands to modal

--- src/view/table/index.js
+++ src/view/table/index.js
@@ -1,12 +1,68 @@
-import React, { useState, useEffect } from 'react'
-
+import React, { useEffect, useMemo } from 'react'
 import PropTypes from 'prop-types'
+
+import { Table } from '@eqworks/react-labs'
+import { Icons, makeStyles, getTailwindConfigColor } from '@eqworks/lumen-labs'
+
 import Accordion from '@material-ui/core/Accordion'
 import AccordionSummary from '@material-ui/core/AccordionSummary'
 import AccordionDetails from '@material-ui/core/AccordionDetails'
 import ExpandMoreIcon from '@material-ui/icons/ExpandMore'
 import { Parser, transforms } from 'json2csv'
-import { Table } from '@eqworks/react-labs'
+
+import { useStoreActions, useStoreState } from '../../store'
+import CustomButton from '../../components/custom-button'
+import CustomModal from '../../components/custom-modal'
+import modes from '../../constants/modes'
+import WidgetMeta from '../meta'
+import CustomToggle from '../../components/custom-toggle'
+
+
+const classes = makeStyles({
+  maximizeButtonContainer: {
+    width: '100%',
+    display: 'flex',
+    justifyContent: 'flex-end',
+  },
+  tableContentContainer: {
+    overflow: 'auto',
+    padding: '1rem',
+  },
+  table: {
+    border: `2px solid ${getTailwindConfigColor('secondary-300')}`,
+    borderRadius: '0.425rem',
+    fontSize: '0.78rem',
+    marginTop: '1rem',
+  },
+  tableExtra: {
+    display: 'flex',
+    '> *': {
+      background: getTailwindConfigColor('secondary-100'),
+      border: `2px solid ${getTailwindConfigColor('secondary-300')}`,
+      borderRadius: '0.425rem',
+      padding: '0.75rem',
+      fontSize: '0.78rem',
+      color: getTailwindConfigColor('secondary-800'),
+      marginRight: '1rem',
+      '&:first-child': {
+        flex: 1,
+      },
+      '&:last-child': {
+        marginRight: 0,
+      },
+    },
+  },
+  tableDisplayControls: {
+    flexShrink: 0,
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'space-around',
+    fontSize: '0.85rem',
+  },
+  tableRawToggle: {
+    fontSize: '0.85rem !important',
+  },
+})
 
 // formerly from util/helpers
 const mapFalsy = {
@@ -79,27 +135,106 @@ const formatCell = ({ value }) => {
   return value
 }
 
-const ResultsTable = ({ results }) => {
-  const [_results, set_results] = useState(results)
-  const tableColumns = _results[0] ? Object.keys(_results[0]) : []
+const ResultsTable = () => {
+  // store actions
+  const update = useStoreActions((actions) => actions.update)
+
+  // widget state
+  const type = useStoreState((state) => state.type)
+  const rows = useStoreState((state) => state.rows)
+  const transformedData = useStoreState((state) => state.transformedData)
+  const domain = useStoreState((state) => state.domain)
+  const renderableValueKeys = useStoreState((state) => state.renderableValueKeys)
+
+  // UI state
+  const mode = useStoreState((state) => state.ui.mode)
+  const tableShowsRawData = useStoreState((state) => state.ui.tableShowsRawData)
+  const maximizeTable = useStoreState((state) => state.ui.maximizeTable)
+
+  const showHeader = useMemo(() => mode === modes.EDITOR || maximizeTable, [maximizeTable, mode])
+  const showMaximizeButton = useMemo(() => mode !== modes.EDITOR, [mode])
+
+  const _results = useMemo(() => tableShowsRawData ? rows : transformedData, [rows, tableShowsRawData, transformedData])
+
+
+  const noTransformedData = useMemo(() => (
+    !type ||
+    !domain.value ||
+    !renderableValueKeys?.length ||
+    !transformedData?.length
+  ), [domain.value, renderableValueKeys?.length, transformedData?.length, type])
+
+
   useEffect(() => {
-    set_results(results)
-  }, [results])
+    if (noTransformedData) {
+      update({ ui: { tableShowsRawData: true } })
+    }
+  }, [noTransformedData, update])
+
+  const renderTable = (
+    <Table
+      data={_results}
+      downloadFn={jsonToCsv}
+    >
+      {Object.keys(_results[0] || {})?.map((d) => (
+        <Table.Column
+          key={d}
+          Header={d}
+          accessor={d}
+          Cell={formatCell}
+        />
+      ))}
+    </Table>
+  )
+
+  const renderTableWithHeader = (
+    showHeader
+      ? <div className={classes.tableContentContainer}>
+        <div className={classes.tableExtra}>
+          <WidgetMeta />
+          <div className={classes.tableDisplayControls}>
+            Display:
+            <CustomToggle
+              disabled={noTransformedData}
+              classes={{
+                label: classes.tableRawToggle,
+              }}
+              value={tableShowsRawData}
+              label='Raw Data'
+              onChange={() => update({ ui: { tableShowsRawData: !tableShowsRawData } })}
+            />
+          </div>
+        </div>
+        <div className={classes.table}>
+          {renderTable}
+        </div>
+      </div>
+      : renderTable
+  )
+  if (maximizeTable) {
+    return (
+      <CustomModal
+        title='Data Overview'
+        onClose={() => update({ ui: { maximizeTable: false } })}
+      >
+        {renderTableWithHeader}
+      </CustomModal>
+    )
+  }
   return (
     <div>
-      <Table
-        data={_results}
-        downloadFn={jsonToCsv}
-      >
-        {tableColumns.map((d) => (
-          <Table.Column
-            key={d}
-            Header={d}
-            accessor={d}
-            Cell={formatCell}
-          />
-        ))}
-      </Table>
+      {
+        showMaximizeButton &&
+        <div className={classes.maximizeButtonContainer}>
+          <CustomButton
+            variant='elevated'
+            onClick={() => update({ ui: { maximizeTable: true } })}
+          >
+            <Icons.Expand size='md' />
+          </CustomButton>
+        </div>
+      }
+      {renderTableWithHeader}
     </div>
   )
 }
