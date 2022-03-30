5776760195503683cdfd59c98d1df39345843a2a
Kyle Grimsrud-Manz <hi@kylegrimsrudma.nz>
2022-03-30 19:42:00 -0400

    tweak - add arbitrary column name replacement according to dict

--- src/util/string-manipulation.js
+++ src/util/string-manipulation.js
@@ -1,3 +1,14 @@
-export const cleanUp = s => s.replace(/_/g, ' ').replace(/./, v => v.toUpperCase())
+const STRING_REPLACE_DICT = {
+  'hh_income_0_50': '0-50%',
+  'hh_income_50_100': '50-100%',
+  'hh_income_100_150': '100-150%',
+  'hh_income_150_200': '150-200%',
+  'hh_income_200_over': '200%+',
+}
+export const cleanUp = s => (
+  s in STRING_REPLACE_DICT
+    ? STRING_REPLACE_DICT[s]
+    : s.replace(/_/g, ' ').replace(/./, v => v.toUpperCase())
+)
 export const getLongestString = arr => arr.reduce((a, b) => (a.length > b.length ? a : b))
 export const isString = v => typeof v === 'string' || v instanceof String
