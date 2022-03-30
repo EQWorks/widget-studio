116b2d7e475b14d8aa027fe8d9fc69ffaed4691b
Kyle Grimsrud-Manz <hi@kylegrimsrudma.nz>
2022-03-30 09:31:46 -0400

    ListDemo - UI tweaks

--- stories/list-demo.js
+++ stories/list-demo.js
@@ -18,7 +18,7 @@ import { deleteWidget, api, createWidget } from '../src/util/api'
 TimeAgo.setDefaultLocale(en.locale)
 TimeAgo.addLocale(en)
 
-const classes = makeStyles({
+const useStyles = ({ widgetPreviewExpanded }) => makeStyles({
   outerContainer: {
     width: '100vw',
     height: '100vh',
@@ -27,10 +27,11 @@ const classes = makeStyles({
   },
   header: {
     background: getTailwindConfigColor('secondary-100'),
-    padding: '1rem',
-    borderRadius: '1rem',
+    padding: '1.4rem 1rem',
+    borderRadius: '0.2rem',
     display: 'flex',
     alignItems: 'center',
+    height: '1rem',
     '> *': {
       marginRight: '0.5rem',
     },
@@ -46,6 +47,8 @@ const classes = makeStyles({
   },
   subHeader: {
     margin: '0.75rem',
+    marginTop: '0.4rem',
+    padding: '0 0.75rem',
     display: 'flex',
     alignItems: 'center',
   },
@@ -56,15 +59,9 @@ const classes = makeStyles({
     fontSize: '0.75rem',
   },
   sortControls: {
-    background: getTailwindConfigColor('secondary-50'),
     display: 'flex',
-    padding: '0.5rem 0.75rem',
-    borderRadius: '0.5rem',
-    borderWidth: '1px',
-    borderColor: getTailwindConfigColor('secondary-400'),
-    '> *': {
-      marginRight: '0.5rem',
-    },
+    height: '100%',
+    padding: '1rem',
     ':last-child': {
       marginRight: '0 !important',
     },
@@ -82,19 +79,12 @@ const classes = makeStyles({
       marginRight: '0 !important',
     },
   },
-  widgetCardGridContainer: {
-    padding: '1rem',
-    flex: 1,
-    display: 'flex',
-    maxHeight: '60%',
-    flexDirection: 'column',
-  },
   widgetCardGrid: {
     padding: '1rem',
     flex: 1,
     width: '100%',
     display: 'grid',
-    gridTemplateColumns: 'repeat(3, 1fr)',
+    gridTemplateColumns: 'repeat(auto-fit, minmax(13rem, 1fr))',
     gridAutoRows: 'min-content',
     gridGap: '1rem',
     overflow: 'auto',
@@ -105,7 +95,6 @@ const classes = makeStyles({
   },
   widgetCard: {
     width: '100%',
-    height: '100%',
     padding: '1rem',
     borderRadius: '1rem',
     borderColor: getTailwindConfigColor('secondary-200'),
@@ -171,11 +160,13 @@ const classes = makeStyles({
       marginBottom: '0 !important',
     },
   },
-  openWidgetContainer: {
-    minHeight: '40%',
-    padding: '1rem',
+  widgetPreviewContainer: {
+    borderColor: getTailwindConfigColor('secondary-200'),
+    borderWidth: '1px',
+    overflow: 'hidden',
+    width: '100%',
   },
-  openWidgetPrompt: {
+  widgetPreviewPrompt: {
     borderRadius: '1rem',
     background: getTailwindConfigColor('secondary-100'),
     color: getTailwindConfigColor('secondary-600'),
@@ -186,6 +177,56 @@ const classes = makeStyles({
   },
   newWidget: {
     width: '100%',
+  },
+  content: {
+    flex: 1,
+    display: 'flex',
+    flexDirection: 'column',
+    width: '100%',
+  },
+  widgetPreviewInnerContainer: {
+    width: '100%',
+    borderTopLeftRadius: '0.5rem',
+    borderTopRightRadius: '0.5rem',
+    height: widgetPreviewExpanded ? '30rem' : '2.85rem',
+    transition: 'all 0.3s',
+    background: getTailwindConfigColor('secondary-50'),
+  },
+  widgetPreview: {
+    height: '2.85rem',
+    padding: '1.25rem',
+    display: 'flex',
+    alignItems: 'center',
+    flex: 1,
+    fontWeight: 700,
+    color: getTailwindConfigColor('secondary-800'),
+    borderBottom: `1px solid ${getTailwindConfigColor('secondary-300')}`,
+    ':not(:first-child)': {
+      marginLeft: '0.2rem',
+    },
+  },
+  widgetPreviewTitle: {
+    flex: 1,
+  },
+  widgetPreviewButton: {
+    background: `${getTailwindConfigColor('secondary-300')} !important`,
+    '& svg': {
+      fill: `${getTailwindConfigColor('secondary-800')} !important`,
+    },
+    '&:hover': {
+      '& svg': {
+        fill: `${getTailwindConfigColor('secondary-900')} !important`,
+      },
+      background: `${getTailwindConfigColor('secondary-400')} !important`,
+    },
+    width: '1.5rem',
+    height: '1.5rem',
+    fontSize: '1.6rem !important',
+    lineHeight: '1.2rem !important',
+    fontWeight: 700,
+    display: 'flex !important',
+    justifyContent: 'center !important',
+    alignItems: 'center !important',
   },
 })
 
@@ -216,9 +257,11 @@ const ListDemo = ({ wl, cu }) => {
   const [currentlyViewing, setCurrentlyViewing] = useState(null)
   const [editMode, setEditMode] = useState(false)
   const [deleteConfirm, setDeleteConfirm] = useState(null)
-
-  const [sortBy, setSortBy] = useState(Object.keys(SORTING)[0])
+  const [widgetPreviewExpanded, setWidgetPreviewExpanded] = useState(false)
   const [sortDescending, setSortDescending] = useState(false)
+  const [sortBy, setSortBy] = useState(Object.keys(SORTING)[0])
+
+  const classes = useStyles({ widgetPreviewExpanded })
 
   const sortedWidgets = useMemo(() => {
     if (widgets?.length) {
@@ -254,7 +297,10 @@ const ListDemo = ({ wl, cu }) => {
       return id && (
         <button
           className={`${classes.widgetCard} ${currentlyViewing === id ? classes.selectedWidgetCard : ''}`}
-          onClick={() => setCurrentlyViewing(id)}>
+          onClick={() => {
+            setCurrentlyViewing(id)
+            setWidgetPreviewExpanded(true)
+          }}>
           <div className={classes.widgetCardHeader}>
             <h1 className={classes.widgetCardTitle}>{title}</h1>
             {
@@ -300,58 +346,58 @@ const ListDemo = ({ wl, cu }) => {
     <>
       <ReactQueryDevtools initialIsOpen={false} />
       <div className={classes.outerContainer}>
-        <div className={classes.widgetCardGridContainer}>
-          <div className={classes.header}>
-            <div className={classes.headerTitle}>
-              Available widgets
+        <div className={classes.header}>
+          <div className={classes.headerTitle}>
+            Available widgets
+          </div>
+          <div className={classes.sortControls}>
+            <div className={classes.control}>
+              <div>
+                Sort by
+              </div>
+              <CustomSelect
+                data={Object.keys(SORTING)}
+                value={sortBy}
+                onSelect={setSortBy}
+              />
             </div>
-            <div className={classes.sortControls}>
-              <div className={classes.control}>
-                <div>
-                  Sort by
-                </div>
-                <CustomSelect
-                  data={Object.keys(SORTING)}
-                  value={sortBy}
-                  onSelect={setSortBy}
-                />
-              </div>
-              <div className={classes.control}>
-                <CustomButton
-                  variant='elevated'
-                  type="secondary"
-                  onClick={() => setSortDescending(!sortDescending)}
-                >
-                  {sortDescending
-                    ? <Icons.ArrowDown />
-                    : <Icons.ArrowUp />}
-                </CustomButton>
-              </div>
+            <div className={classes.control}>
+              <CustomButton
+                variant='elevated'
+                type="secondary"
+                onClick={() => setSortDescending(!sortDescending)}
+              >
+                {sortDescending
+                  ? <Icons.ArrowDown />
+                  : <Icons.ArrowUp />}
+              </CustomButton>
             </div>
           </div>
-          <div className={classes.subHeader}>
-            <div className={classes.subHeaderText}>
-              {widgetsDescription}
-            </div>
-            <CustomButton
-              horizontalMargin
-              disabled={loading}
-              variant='outlined'
-              onClick={refetch}
-              endIcon={<Icons.Cycle size='sm' />}
-            >
-              REFRESH
-            </CustomButton>
-            <CustomButton
-              disabled={!widgets?.length}
-              variant='outlined'
-              type={editMode ? 'error' : 'primary'}
-              onClick={() => setEditMode(!editMode)}
-              endIcon={<Icons.Edit size='sm' />}
-            >
-              {editMode ? 'DONE' : 'EDIT'}
-            </CustomButton>
+        </div>
+        <div className={classes.subHeader}>
+          <div className={classes.subHeaderText}>
+            {widgetsDescription}
           </div>
+          <CustomButton
+            horizontalMargin
+            disabled={loading}
+            variant='outlined'
+            onClick={refetch}
+            endIcon={<Icons.Cycle size='sm' />}
+          >
+            REFRESH
+          </CustomButton>
+          <CustomButton
+            disabled={!widgets?.length}
+            variant='outlined'
+            type={editMode ? 'error' : 'primary'}
+            onClick={() => setEditMode(!editMode)}
+            endIcon={<Icons.Edit size='sm' />}
+          >
+            {editMode ? 'DONE' : 'EDIT'}
+          </CustomButton>
+        </div>
+        <div className={classes.content}>
           <div className={classes.widgetCardGrid}>
             {
               loading
@@ -376,19 +422,41 @@ const ListDemo = ({ wl, cu }) => {
                 </>
             }
           </div>
-        </div>
-        <div className={classes.openWidgetContainer}>
-          {
-            currentlyViewing
-              ? <Widget
-                key={currentlyViewing}
-                id={currentlyViewing}
-                mode='view_only'
-              />
-              : <div className={classes.openWidgetPrompt} >
-                Select a widget to view it.
+          <div className={classes.widgetPreviewContainer}>
+            <div
+              className={classes.widgetPreviewInnerContainer}
+            >
+              <div className={classes.widgetPreview}>
+                <span className={classes.widgetPreviewTitle}>
+                  Widget Preview:
+                </span>
+                <CustomButton
+                  classes={{
+                    button: classes.widgetPreviewButton,
+                  }}
+                  type='secondary'
+                  onClick={() => setWidgetPreviewExpanded(!widgetPreviewExpanded)}
+                >
+                  {
+                    widgetPreviewExpanded
+                      ? <Icons.Remove size='sm' />
+                      : <Icons.Add size='sm' />
+                  }
+                </CustomButton>
               </div>
-          }
+              {
+                currentlyViewing
+                  ? <Widget
+                    key={currentlyViewing}
+                    id={currentlyViewing}
+                    mode='view_only'
+                  />
+                  : <div className={classes.widgetPreviewPrompt} >
+                    Select a widget to view it.
+                  </div>
+              }
+            </div>
+          </div>
         </div>
       </div>
     </>
