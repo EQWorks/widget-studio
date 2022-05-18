50ead77c831fa95554bbd9e198e1d31f3b248ae7
Erika Szabo <erika.szabo.geo@gmail.com>
2022-05-17 21:11:47 -0400

    util - add getLayerValueKeys

--- src/util/index.js
+++ src/util/index.js
@@ -1,4 +1,9 @@
-import { MAP_LAYER_GEO_KEYS, COORD_KEYS } from '../constants/map'
+import {
+  MAP_LAYER_VALUE_VIS,
+  MAP_LAYER_GEO_KEYS,
+  COORD_KEYS,
+  EXCLUDE_NUMERIC,
+} from '../constants/map'
 
 
 // TO DO: extend conditions for polygon geojson & arc layers when implementing those layers as well
@@ -9,3 +14,13 @@ export const geoKeyHasCoordinates = (geoKey, numericColumns) =>
   (MAP_LAYER_GEO_KEYS.geojson.includes(geoKey))
 
 export const isObject = v => v !== null && !Array.isArray(v) && typeof v === 'object' && !(v instanceof HTMLElement)
+
+export const getLayerValueKeys = ({ mapValueKeys, dataKeys, data, layer }) => {
+  let layerValueKeys = mapValueKeys.filter(o => MAP_LAYER_VALUE_VIS[layer].includes(o.mapVis))
+    .map(o => o.title)
+  if (!layerValueKeys.length) {
+    layerValueKeys =  dataKeys.filter(col => EXCLUDE_NUMERIC.every(key => !col.includes(key)) &&
+      !isNaN(data?.[0]?.[col]))
+  }
+  return layerValueKeys
+}
