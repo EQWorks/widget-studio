a4c012705ace367772ad61cdbe50187cbbc7a09e
Erika Szabo <erika.szabo.geo@gmail.com>
2022-05-19 20:05:50 -0400

    XWIReportValueControls - add Hide CustomToggle & MutedBarrier to each layer controls

--- src/controls/shared/map-value-controls/xwi-report-value-controls.js
+++ src/controls/shared/map-value-controls/xwi-report-value-controls.js
@@ -1,47 +1,87 @@
 // specific value control component for xwi report data
-import React from 'react'
+import React, { useMemo } from 'react'
 import PropTypes from 'prop-types'
 
-import { makeStyles } from '@eqworks/lumen-labs'
+import { makeStyles, getTailwindConfigColor } from '@eqworks/lumen-labs'
 
+import MutedBarrier from '../muted-barrier'
 import MapValueSelect from './map-value-select'
-import { useStoreState } from '../../../store'
+import CustomToggle from '../../../components/custom-toggle'
+import { useStoreActions, useStoreState } from '../../../store'
 import { MAP_LAYER_VALUE_VIS } from '../../../constants/map'
 
 
 const classes = makeStyles({
-  layerValueControls:{
+  layerValueControls: {
     marginBottom: '1rem',
+  },
+  layer: {
+    display: 'flex',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+  },
+  layerHeader: {
+    fontWeight: 700,
+    color: getTailwindConfigColor('secondary-800'),
   },
 })
 
-const XWI_MAP_LAYERS = [
-  {
-    categories: MAP_LAYER_VALUE_VIS.scatterplot,
-    header: 'Source Layer',
-  },
-  {
-    categories: MAP_LAYER_VALUE_VIS.targetScatterplot,
-    header: 'Target Layer',
-  },
-  {
-    categories: MAP_LAYER_VALUE_VIS.arc,
-    header: 'Arc Layer',
-  },
-]
-
 const XWIReportValueControls = ({ data, callback }) => {
+  // store actions
+  const userUpdate = useStoreActions((actions) => actions.update)
+
+  // widget state
   const mapValueKeys = useStoreState((state) => state.mapValueKeys)
+  const genericOptions = useStoreState((state) => state.genericOptions)
+
+  const XWI_MAP_LAYERS = useMemo(() => (
+    [
+      {
+        categories: MAP_LAYER_VALUE_VIS.scatterplot,
+        header: 'Source Layer',
+        switchLabel: 'mapHideSourceLayer',
+        switchState: genericOptions.mapHideSourceLayer,
+      },
+      {
+        categories: MAP_LAYER_VALUE_VIS.targetScatterplot,
+        header: 'Target Layer',
+        switchLabel: 'mapHideTargetLayer',
+        switchState: genericOptions.mapHideTargetLayer,
+      },
+      {
+        categories: MAP_LAYER_VALUE_VIS.arc,
+        header: 'Arc Layer',
+        switchLabel: 'mapHideArcLayer',
+        switchState: genericOptions.mapHideArcLayer,
+      },
+    ]
+  ),[genericOptions.mapHideSourceLayer, genericOptions.mapHideTargetLayer, genericOptions.mapHideArcLayer])
 
   return (
-    XWI_MAP_LAYERS.map(({ categories, header }, i) => (
+    XWI_MAP_LAYERS.map(({ categories, header, switchLabel, switchState }, i) => (
       <div key={i} className={classes.layerValueControls}>
-        <p>{header}</p>
-        <MapValueSelect
-          categories={categories}
-          values={mapValueKeys}
-          {...{ data, callback }}
-        />
+        <div className={classes.layer}>
+          <div className={classes.layerHeader}>
+            {header}
+          </div>
+          <CustomToggle
+            disabled={false}
+            value={switchState}
+            label='Hide'
+            onChange={() => {
+              userUpdate({ genericOptions: { [switchLabel]: !switchState } })
+            }}
+          />
+        </div>
+        <MutedBarrier
+          mute={switchState}
+        >
+          <MapValueSelect
+            categories={categories}
+            values={mapValueKeys}
+            {...{ data, callback }}
+          />
+        </MutedBarrier>
       </div>
     ))
   )
