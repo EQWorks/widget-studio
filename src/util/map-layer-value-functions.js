5f72c1228db3bd6d0921e79dfd55d63a4d980258
Erika Szabo <erika.szabo.geo@gmail.com>
2022-06-09 22:15:33 -0400

    util/map-layer-value-functions - replace every with some in getLayerValueKeys & change import source for EXCLUDE_NUMERIC

--- src/util/map-layer-value-functions.js
+++ src/util/map-layer-value-functions.js
@@ -1,11 +1,12 @@
-import { MAP_LAYER_VALUE_VIS, EXCLUDE_NUMERIC } from '../constants/map'
+import { MAP_LAYER_VALUE_VIS } from '../constants/map'
+import { EXCLUDE_NUMERIC } from '../constants/columns'
 
 
 export const getLayerValueKeys = ({ mapValueKeys, dataKeys, data, layer }) => {
   let layerValueKeys = mapValueKeys.filter(o => MAP_LAYER_VALUE_VIS[layer].includes(o.mapVis))
     .map(o => o.title)
   if (!layerValueKeys.length) {
-    layerValueKeys =  dataKeys.filter(col => EXCLUDE_NUMERIC.every(key => !col.includes(key)) &&
+    layerValueKeys = dataKeys.filter(col => !EXCLUDE_NUMERIC.some(key => key === col) &&
       !isNaN(data?.[0]?.[col]))
   }
   return layerValueKeys
