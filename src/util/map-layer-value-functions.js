b003af6441435fb915ddf5228eaf0acc6ed93a7c
Erika Szabo <erika.szabo.geo@gmail.com>
2022-06-21 13:18:58 -0400

    util/map-layer-value-functions - improve getLayerValueKeys by excluding values from EXCLUDE_NUMERIC_ENDINGS

--- src/util/map-layer-value-functions.js
+++ src/util/map-layer-value-functions.js
@@ -1,17 +1,20 @@
 import { MAP_LAYER_VALUE_VIS } from '../constants/map'
-import { EXCLUDE_NUMERIC } from '../constants/columns'
+import { EXCLUDE_NUMERIC, EXCLUDE_NUMERIC_ENDINGS } from '../constants/columns'
+import { cleanUp } from './string-manipulation'
 
-
+// used for Tooltip list of data { key: value } to display for xwi report when no data visualization is selected
 export const getLayerValueKeys = ({ mapValueKeys, dataKeys, data, layer }) => {
   let layerValueKeys = mapValueKeys.filter(o => MAP_LAYER_VALUE_VIS[layer].includes(o.mapVis))
     .map(o => o.title)
   if (!layerValueKeys.length) {
-    layerValueKeys = dataKeys.filter(col => !EXCLUDE_NUMERIC.some(key => key === col) &&
-      !isNaN(data?.[0]?.[col]))
+    layerValueKeys = dataKeys.filter(col => !isNaN(data?.[0]?.[col]) &&
+      !EXCLUDE_NUMERIC.some(key => cleanUp(key) === col || key === col) &&
+      !EXCLUDE_NUMERIC_ENDINGS.some(key => col.endsWith(key)))
   }
   return layerValueKeys
 }
 
+// setup mapValueKeys list of map visualizations with empty values
 export const setMapValueKeys = ({ mapLayer, dataIsXWIReport }) => {
   return (dataIsXWIReport ?
     [
