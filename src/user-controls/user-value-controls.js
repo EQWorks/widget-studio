8be2bc3365d28786a0ba9bd403b2f7860ec7e306
Erika Szabo <erika.szabo.geo@gmail.com>
2022-06-29 17:36:39 -0400

    user-controls - create new component UserValueControls

---
+++ src/user-controls/user-value-controls.js
@@ -0,0 +1,76 @@
+import React from 'react'
+
+import { makeStyles, getTailwindConfigColor } from '@eqworks/lumen-labs'
+
+import { useStoreState, useStoreActions } from '../store'
+
+
+const classes = makeStyles({
+  benchmarkContainer: {
+    display: 'flex',
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+    gap: '3rem',
+    height: '4rem',
+    background: getTailwindConfigColor('secondary-50'),
+    // boxShadow: `0 0 0.2rem ${getTailwindConfigColor('shadow-blue-20')}`,
+    // HOW to add opacity with hex color from getTailwindConfigColor???
+    boxShadow: '0px 0.125rem 0.5rem rgba(54, 111, 228, 0.1)',
+  },
+  benchmarkItems: {
+    fontFamily: 'Open Sans',
+    fontStyle: 'normal',
+    fontWeight: '700',
+    fontSize: '1.123rem',
+    lineHeight: '1.875rem',
+    // letterSpacing: '0.015625rem',
+    textTransform: 'capitalize',
+    color: getTailwindConfigColor('secondary-900'),
+    outline: 'none',
+    '&:focus': {
+      outline: 'none',
+      color: getTailwindConfigColor('primary-500'),
+      borderBottom: `0.125rem solid ${getTailwindConfigColor('primary-500')}`,
+    },
+    '&:hover': {
+      outline: 'none',
+    },
+  },
+})
+
+const UserValueControls = () => {
+  const userUpdate = useStoreActions(actions => actions.userUpdate)
+  const benchmarkHeadline = useStoreState((state) => state.benchmarkHeadline)
+  const benchmarkKeyValues = useStoreState((state) => state.benchmarkKeyValues)
+  const formattedColumnNames = useStoreState((state) => state.formattedColumnNames)
+  const valueKeys = useStoreState((state) => state.valueKeys)
+
+  return (
+    <div className={classes.benchmarkContainer}>
+      {benchmarkHeadline &&
+        <div className={classes.benchmarkItems}>
+          {benchmarkHeadline}:
+        </div>
+      }
+      {benchmarkKeyValues.map((dataKey, i) => (
+        <button
+          key={i}
+          className={classes.benchmarkItems}
+          onClick={() => {
+            if (valueKeys.length > 1) {
+              valueKeys.pop()
+            }
+            const valueKeysCopy = JSON.parse(JSON.stringify(valueKeys))
+            valueKeysCopy.push({ key: dataKey, title: formattedColumnNames[dataKey], agg: 'unique' })
+            userUpdate({ valueKeys: valueKeysCopy })
+          }}
+        >
+          {formattedColumnNames[dataKey]}
+        </button>
+      ))}
+    </div>
+  )
+}
+
+export default UserValueControls
