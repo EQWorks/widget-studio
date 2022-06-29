deea64e1f5366f085f06f3aed441737cbdb896de
Erika Szabo <erika.szabo.geo@gmail.com>
2022-06-29 17:39:09 -0400

    LeftSidebar - add BenchmarkControls for bar charts and only when some conditions are met

--- src/controls/editor-mode/left-sidebar.js
+++ src/controls/editor-mode/left-sidebar.js
@@ -10,11 +10,16 @@ import MapValueControls from '../shared/map-value-controls'
 import EditorSidebarBase from './sidebar-base'
 import DataTransformationControls from '../shared/data-transformation-controls'
 import DataSourceControls from './components/data-source-controls'
+import BenchmarkControls from './components/benchmark-controls'
 
 
 const EditorLeftSidebar = () => {
   const type = useStoreState((state) => state.type)
   const dataIsXWIReport = useStoreState((state) => state.dataIsXWIReport)
+  const uniqueOptions = useStoreState((state) => state.uniqueOptions)
+  const numericColumns = useStoreState((state) => state.numericColumns)
+  const addBenchmark = useStoreState((state) => state.addBenchmark)
+  const renderableValueKeys = useStoreState((state) => state.renderableValueKeys)
 
   return (
     <EditorSidebarBase isLeft>
@@ -30,6 +35,12 @@ const EditorLeftSidebar = () => {
             <DomainControls />
             <ValueControls />
             {type !== types.PYRAMID && <DataTransformationControls />}
+            <DataTransformationControls />
+            {/* TO DO: restrict to dev only for now */}
+            { type === types.BAR && !uniqueOptions.stacked && numericColumns.length > 1 &&
+              ((renderableValueKeys.length <= 1 && !addBenchmark) || addBenchmark) &&
+              <BenchmarkControls />
+            }
           </>
       }
     </EditorSidebarBase>
