fa1aa9dd62216138628056bad350ba0dea861355
Erika Szabo <erika.szabo.geo@gmail.com>
2022-07-06 22:48:30 -0400

    constants/insights-data-categories - add DATA_CATEGORIES, DATA_CATEGORIES_KEYS, & DATA_CATEGORIES_VALUES to constants

---
+++ src/constants/insights-data-categories.js
@@ -0,0 +1,12 @@
+export const DATA_CATEGORIES = {
+  families: [
+    'census_fam_lone_parent',
+    'census_fam_couple_with_no_children',
+    'census_fam_couple_with_children',
+    'census_fam_person_not_in_census_family',
+  ],
+}
+
+export const DATA_CATEGORIES_KEYS = Object.keys(DATA_CATEGORIES)
+
+export const DATA_CATEGORIES_VALUES = Object.values(DATA_CATEGORIES).flat()
