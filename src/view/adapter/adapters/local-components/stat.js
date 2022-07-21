07b231d9aa46ee8a128db2a6048cddfb4e9f7786
kc-leung <chiu-leung@hotmail.com>
2022-07-21 15:23:42 -0400

    stat-widget - add innerContainer class styling

--- src/view/adapter/adapters/local-components/stat.js
+++ src/view/adapter/adapters/local-components/stat.js
@@ -13,6 +13,7 @@ const classes = makeStyles({
     width: '100%',
     height: '100%',
     display: 'flex',
+    flexDirection: 'column',
     alignItems: 'center',
     justifyContent: 'center',
 
