17b6b41f1317c2aae53ee557efa618c9974bfae8
Erika Szabo <erika.szabo.geo@gmail.com>
2022-07-28 20:44:07 +0300

    LeftSidebar - add toggle for top categories

--- src/controls/editor-mode/left-sidebar.js
+++ src/controls/editor-mode/left-sidebar.js
@@ -1,6 +1,6 @@
 import React from 'react'
 
-import { useStoreState } from '../../store'
+import { useStoreState, useStoreActions } from '../../store'
 import types from '../../constants/types'
 import WidgetTypeControls from '../shared/type-controls'
 import DomainControls from '../shared/domain-controls'
@@ -11,14 +11,19 @@ import EditorSidebarBase from './sidebar-base'
 import DataTransformationControls from '../shared/data-transformation-controls'
 import DataSourceControls from './components/data-source-controls'
 import UserValueConfigurationControls from './components/user-value-configuration-controls'
+import WidgetControlCard from '../shared/components/widget-control-card'
+import { renderToggle } from '../shared/util'
 import { hasDevAccess  } from '../../util/access'
 
 
 const EditorLeftSidebar = () => {
+  const userUpdate = useStoreActions(actions => actions.userUpdate)
+
   const type = useStoreState((state) => state.type)
   const dataIsXWIReport = useStoreState((state) => state.dataIsXWIReport)
   const numericColumns = useStoreState((state) => state.numericColumns)
   const addUserControls = useStoreState((state) => state.addUserControls)
+  const addTopCategories = useStoreState((state) => state.addTopCategories)
   const renderableValueKeys = useStoreState((state) => state.renderableValueKeys)
 
   return (
@@ -35,8 +40,18 @@ const EditorLeftSidebar = () => {
             <DomainControls />
             <ValueControls />
             {type !== types.PYRAMID && <DataTransformationControls />}
-            {/* restrict to dev only for now */}
           </>
+      }
+      {/* restrict to dev only for now */}
+      {hasDevAccess() && [types.BAR, types.LINE].includes(type) && !addUserControls &&
+        <WidgetControlCard title='Top Categories'>
+          {
+            renderToggle('Add Top Categories',
+              addTopCategories,
+              () => userUpdate({ addTopCategories: !addTopCategories }),
+            )
+          }
+        </WidgetControlCard>
       }
       {hasDevAccess() && ((type == types.BAR && numericColumns.length > 1 &&
         ((renderableValueKeys.length <= 1 && !addUserControls) || addUserControls)) ||
