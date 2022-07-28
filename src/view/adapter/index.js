d3ea0c8f2a700fbeefec6a42016bd86b8cdb7454
Erika Szabo <erika.szabo.geo@gmail.com>
2022-07-28 20:33:31 +0300

    WidgetAdapter - add TopCategories component

--- src/view/adapter/index.js
+++ src/view/adapter/index.js
@@ -8,15 +8,17 @@ import { useStoreActions, useStoreState } from '../../store'
 import typeInfo from '../../constants/type-info'
 import types from '../../constants/types'
 import UserValueControls from '../../user-controls/user-value-controls'
+import TopCategories from '../../user-controls/top-categories'
 
 
-const useStyles = ({ type, renderUserControlValues }) => makeStyles({
+const useStyles = ({ type, renderUserControlValues, addTopCategories }) => makeStyles({
   container: {
     position: 'relative',
     width: '100%',
     height: '100%',
   },
   widget: {
+    ...(addTopCategories && { display: 'flex' }),
     // 64px is the height of the WidgetValueControls container
     height: renderUserControlValues ? 'calc(100% - 64px)' : '100%',
     padding: renderUserControlValues && type !== types.MAP ? '1rem' : 0,
@@ -48,6 +50,7 @@ const WidgetAdapter = () => {
   const transformedData = useStoreState((state) => state.transformedData)
   const renderableValueKeys = useStoreState((state) => state.renderableValueKeys)
   const addUserControls = useStoreState((state) => state.addUserControls)
+  const addTopCategories = useStoreState((state) => state.addTopCategories)
   const userControlKeyValues = useStoreState((state) => state.userControlKeyValues)
   const selectedUserDataControlIndex = useStoreState((state) => state.selectedUserDataControlIndex)
   const categoryKeyValues = useStoreState((state) => state.categoryKeyValues)
@@ -66,7 +69,7 @@ const WidgetAdapter = () => {
     (userControlKeyValues.length > 0))
   , [addUserControls, userControlKeyValues])
 
-  const classes = useStyles({ type, renderUserControlValues })
+  const classes = useStyles({ type, renderUserControlValues, addTopCategories })
 
   // memoize the correct adapter
   const { component, adapt } = useMemo(() => typeInfo[type].adapter, [type])
@@ -83,6 +86,9 @@ const WidgetAdapter = () => {
         <UserValueControls/>
       }
       <div className={classes.widget}>
+        {addTopCategories && !addUserControls &&
+          <TopCategories/>
+        }
         {Boolean(addUserControls && categoryKeyValues?.length && selectedUserDataControlIndex >= 0) && (
           <div className={classes.userValueDropdownSelect}>
             {userValueDropdownSelect}
