b92fec76aa653ee2f92da765146a3cb826c028ce
Erika Szabo <erika.szabo.geo@gmail.com>
2022-08-05 14:15:06 +0200

    constants/data-format -  add topCox and others to DATA_KEY_FORMATTING

--- src/constants/data-format.js
+++ src/constants/data-format.js
@@ -7,9 +7,32 @@ export const DATA_KEY_FORMATTING = {
     keyList: ['cost', 'spend', 'revenue'],
     formatFunction: val => numeral(val).format('$0,0.00'),
   },
+  money: {
+    keyList: ['income'],
+    formatFunction: val => numeral(val).format('$0,0'),
+  },
   percentage: {
     keyList: ['rate', 'ctr'],
     formatFunction: val => numeral(val).format('0.00%'),
+  },
+  topCox: {
+    keyList: [
+      'arts_aficionados', 'health_and_beauty', 'small_business_owners', 'readers', 'car_owner', 'students',
+      'small_business_insights', 'online_savvy_shoppers', 'home_owners', 'movies_goers', 'diyers',
+      'looking_for_love', 'big_grocery_spenders', 'online_cord', 'big_medical_spenders', 'sport_and_outdoor_enthusiasts',
+      'gamers', 'big_telco_spenders', 'value_conscious_shoppers', 'gamblers', 'frequent_travellers',
+      'families_and_children', 'early_tech_adopters', 'fashion_buyers', 'pet_owners', 'fitness_enthusiasts',
+      'dining_out',
+    ],
+    formatFunction: val => numeral(val).format('%'),
+  },
+  others: {
+    keyList: ['transit', 'approved', 'degree', 'value', 'minivans', 'honda', 'public transit'],
+    formatFunction: val => val * 100 + '%',
+  },
+  distance_others: {
+    keyList: ['distance'],
+    formatFunction: val =>  val + 'km',
   },
   numericOthers: {
     keyList: [],
