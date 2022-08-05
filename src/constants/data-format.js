369e4896bb186f92f3eba0eafc819f52eddbf6a7
Erika Szabo <erika.szabo.geo@gmail.com>
2022-08-05 14:15:06 +0200

    constants/data-format - add to DATA_KEY_FORMATTING

--- src/constants/data-format.js
+++ src/constants/data-format.js
@@ -7,9 +7,21 @@ export const DATA_KEY_FORMATTING = {
     keyList: ['cost', 'spend', 'revenue'],
     formatFunction: val => numeral(val).format('$0,0.00'),
   },
+  money: {
+    keyList: ['income'],
+    formatFunction: val => numeral(val).format('$0,0'),
+  },
   percentage: {
     keyList: ['rate', 'ctr'],
     formatFunction: val => numeral(val).format('0.00%'),
+  },
+  others: {
+    keyList: ['transit', 'approved', 'degree', 'value', 'minivans', 'honda', 'public transit'],
+    formatFunction: val => val * 100 + '%',
+  },
+  distance_others: {
+    keyList: ['distance'],
+    formatFunction: val =>  val + 'km',
   },
   numericOthers: {
     keyList: [],
