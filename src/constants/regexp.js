a438a20ce4cea0b6ff062a78825f00665f88d7ab
Erika Szabo <erika.szabo.geo@gmail.com>
2022-09-09 11:43:02 -0400

    constants/regexp - create file with regexp constants

---
+++ src/constants/regexp.js
@@ -0,0 +1,5 @@
+export const yearMonthClientRegExp = new RegExp(/[_][0-9]{6}[_][0-9]+$/g)
+
+export const clientIdRegExp = new RegExp(/[_][0-9]+$/g)
+
+export const reportYMRegExp = new RegExp(/[_][0-9]{6}[_]/g)
