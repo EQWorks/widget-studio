6e50c7737b869a5d32ef57ffe2344cac7d62334c
Erika Szabo <erika.szabo.geo@gmail.com>
2022-09-16 09:35:25 -0400

    util/string-manipulation - add new keys to STRING_REPLACE_DICT; clean up unused keys

--- src/util/string-manipulation.js
+++ src/util/string-manipulation.js
@@ -1,23 +1,11 @@
 export const STRING_REPLACE_DICT = {
   'pop': 'Population',
-  'income_sources_total_income_recepients': 'Total income recepients',
   'income_sources_total_income_avg': 'Average income',
-  'income_sources_market_income_recipients': 'Market income',
-  'income_sources_government_transfer_recipients': 'Government transfer income',
-  'income_sources_employment_income_recipients': 'Employment income',
-  'income_sources_market_income_avg': 'Market income',
-  'income_sources_government_transfer_avg': 'Government transfer income',
-  'income_sources_employment_income_avg': 'Employment income',
   'census_fam_couple_with_no_children': 'Couple - no children',
-  'census_fam_couple_with_children': 'Couple - children',
-  'census_fam_person_not_in_census_family': 'Not in census family',
-  'commute_time_60+min': '> 60 min',
-  'dta': 'DTA',
-  'aoi': 'AOI',
-  'da': 'DA',
+  'census_fam_couple_with_children': 'Couple - with children',
+  'census_fam_person_not_in_census_family': 'Other',
   'Top Spending Geography': 'Top Spending',
-  'Frequently Transacting Geography': 'Frequently Transacting',
-  'Minivans': 'Minivan',
+  'Frequently Transacting Geography': 'Transaction Frequency',
   'value_conscious_shoppers': 'Dining out',
   'dealersaleprice': 'Dealer',
   'dealerreserveprice': 'Dealer',
@@ -25,32 +13,23 @@ export const STRING_REPLACE_DICT = {
   'dmasaleprice': 'DMA',
   'dmareserveprice': 'DMA',
   'dmareservepct': 'DMA',
+  'hh_income_0_50k': '$0 - $50k',
+  'hh_income_50_100k': '$50 - $100k',
+  'hh_income_100_150k': '$100 - $150k',
+  'hh_income_150_200k': '$150 - $200k',
+  'hh_income_200k+': '$200k+',
 }
 
 const HEADER_START = [
   'pop_',
-  'hh_income_',
   'highest_education_',
   'census_fam_',
-  'hh_tenure_',
-  'commute_time_',
-  'commute_mode_',
-  'visits_hod_',
-  'visits_dow_',
-  'unique_visitors_hod_',
-  'unique_visitors_dow_',
 ]
 
 const customCleanUp = s => {
   const header = HEADER_START.find(h => s?.toString().startsWith(h))
   if (header && !STRING_REPLACE_DICT[s]) {
     const label = s?.toString().replace(header, '').replace(/./, v => v.toUpperCase())
-    if (header.includes('hh_income')) {
-      return label.replace('_', '-')
-    }
-    if (header.includes('hod')) {
-      return label + 'h'
-    }
     return label.replaceAll('_', ' ')
   }
   return STRING_REPLACE_DICT[s]
