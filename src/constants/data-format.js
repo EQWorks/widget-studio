c69ee0fe0fa010aaa1d8161cfb87ab75761b914a
Erika Szabo <erika.szabo.geo@gmail.com>
2022-09-16 09:30:42 -0400

    constants/data-format - add new keys to percentageList; change percentage format function

--- src/constants/data-format.js
+++ src/constants/data-format.js
@@ -1,8 +1,10 @@
 import numeral from 'numeral'
 import { quickNumericFormat } from '../util/numeric'
+import { DATA_CATEGORIES } from './insights-data-categories'
 
 
 export const DATA_KEY_FORMATTING = {
+
   currency : {
     keyList: ['cost', 'spend', 'revenue'],
     formatFunction: val => numeral(val).format('$0,0.00'),
@@ -12,8 +14,16 @@ export const DATA_KEY_FORMATTING = {
     formatFunction: val => numeral(val).format('$0,0'),
   },
   percentage: {
-    keyList: ['rate', 'ctr'],
-    formatFunction: val => numeral(val).format('0.00%'),
+    keyList: ['rate', 'ctr',
+      ...DATA_CATEGORIES['Vehicle Type'],
+      ...DATA_CATEGORIES['Vehicle Make'],
+      ...DATA_CATEGORIES['Vehicle Age'],
+      'Marketshare of vehicle age', 'OEM Auto Brand',
+      'Dealer Market Area', 'Market area', 'City', 'Province', 'Country', 'Trend Difference',
+      'Submitted Deals', 'Avg Submitted Deals in the DMA', 'Approval Rate', 'Avg Approval Rate in the DMA', 'Funding Rate', 'Avg Funding Rate in the DMA', 'dealer', 'dma',
+      'hh_income_0_50k', 'hh_income_50_100k', 'hh_income_100_150k', 'hh_income_150_200k', 'hh_income_200k+', 'male', 'female',
+    ],
+    formatFunction: val => numeral(val/100).format('0.00%'),
   },
   topCox: {
     keyList: [
@@ -24,7 +34,7 @@ export const DATA_KEY_FORMATTING = {
       'families_and_children', 'early_tech_adopters', 'fashion_buyers', 'pet_owners', 'fitness_enthusiasts',
       'dining_out',
     ],
-    formatFunction: val => numeral(val).format('%'),
+    formatFunction: val => numeral(val / 100).format('%'),
   },
   others: {
     keyList: ['transit', 'approved', 'degree', 'value', 'minivans', 'honda', 'public transit'],
