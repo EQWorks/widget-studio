f5f6f5122682f47435803610a3767c1041ce5633
Erika Szabo <erika.szabo.geo@gmail.com>
2022-10-20 15:52:18 -0400

    Controls/MapDomainControls - enable editing for WidgetControlCard; add TextField comp for column title alias

--- src/controls/shared/map-domain-controls.js
+++ src/controls/shared/map-domain-controls.js
@@ -1,13 +1,19 @@
 import React from 'react'
 
+import { TextField, Icons } from '@eqworks/lumen-labs'
+
 import { useStoreState, useStoreActions } from '../../store'
 import CustomSelect from '../../components/custom-select'
 import WidgetControlCard from '../shared/components/widget-control-card'
-import { renderRow } from './util'
-import { MAP_LAYERS, MAP_LAYER_GEO_KEYS } from '../../constants/map'
+import { renderRow, renderItem } from './util'
 import { setMapValueKeys } from '../../util/map-layer-value-functions'
-import { Icons } from '@eqworks/lumen-labs'
+import { hasDevAccess } from '../../util/access'
+import { MAP_LAYERS, MAP_LAYER_GEO_KEYS } from '../../constants/map'
 
+
+const textfieldClasses = Object.freeze({
+  container: 'mt-0.5',
+})
 
 const MapDomainControls = () => {
   // common actions
@@ -19,8 +25,9 @@ const MapDomainControls = () => {
   const domain = useStoreState((state) => state.domain)
   const mapLayer = useStoreState((state) => state.mapLayer)
   const dataIsXWIReport = useStoreState((state) => state.dataIsXWIReport)
+  const widgetControlCardEdit = useStoreState((state) => state.widgetControlCardEdit)
 
-  const renderControls = (
+  const renderControls = renderItem('Column', (
     <CustomSelect
       fullWidth
       data={validMapGroupKeys}
@@ -32,9 +39,9 @@ const MapDomainControls = () => {
         const newLayer = Object.keys(MAP_LAYERS)
           .find(layer => MAP_LAYER_GEO_KEYS[layer].includes(val))
         /*
-         * reset mapValueKeys when we change to a mapGroupKey that requires a different layer,
-         * as different layer requires different visualization types
-         */
+        * reset mapValueKeys when we change to a mapGroupKey that requires a different layer,
+        * as different layer requires different visualization types
+        */
         if (newLayer !== mapLayer) {
           update({ mapValueKeys: setMapValueKeys({ mapLayer, dataIsXWIReport }) || [] })
         }
@@ -47,11 +54,31 @@ const MapDomainControls = () => {
       })}
       placeholder={'Select a column to group by'}
     />
-  )
+  ))
+
+  const renderAlias = renderItem('Alias', (
+    <TextField
+      classes={textfieldClasses}
+      size={'md'}
+      // value={showAxisTitles.y ? axisTitles.y : 'N/A'}
+      inputProps={{ placeholder: 'Column title alias' }}
+      // onChange={(val) => userUpdate({ genericOptions: { axisTitles: { y: val } } })}
+      // maxLength={100}
+      disabled={!domain.value}
+    />
+  ))
 
   return (
-    <WidgetControlCard title={'Map Layer Configuration'}>
-      {renderRow('Column', renderControls)}
+    <WidgetControlCard
+      title={'Map Layer Configuration'}
+      enableEdit={hasDevAccess() && domain.value}
+    >
+      {renderRow('',
+        <>
+          {renderControls}
+          {widgetControlCardEdit && renderAlias}
+        </>
+      )}
     </WidgetControlCard>
   )
 }
