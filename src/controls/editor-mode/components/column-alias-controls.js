f837859bfe94939e92c3072f9b71ccdff47a1664
Erika Szabo <erika.szabo.geo@gmail.com>
2022-10-24 19:42:21 -0400

    EditorMode/ColumnAliasControls - add alias validation

--- src/controls/editor-mode/components/column-alias-controls.js
+++ src/controls/editor-mode/components/column-alias-controls.js
@@ -1,22 +1,36 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState, useMemo } from 'react'
 import PropTypes from 'prop-types'
 
 import { useDebounce } from 'use-debounce'
 import { TextField } from '@eqworks/lumen-labs'
-import { useStoreActions } from '../../../store'
+import { useStoreActions, useStoreState } from '../../../store'
 
 
 const textfieldClasses = Object.freeze({
-  container: 'h-8',
+  container: 'mt-0.5',
 })
 
 const ColumnAliasControls = ({ value, disabled }) => {
   const userUpdate = useStoreActions((actions) => actions.userUpdate)
-  const [alias, setAlias] = useState('')
+  const columnNameAliases = useStoreState((state) => state.columnNameAliases || {})
+  const [alias, setAlias] = useState(columnNameAliases[value])
   const [debouncedAlias] = useDebounce(alias, 1000)
 
-  useEffect(() => userUpdate({ columnNameAliases: { [value]: debouncedAlias } }),
-    [userUpdate, value, debouncedAlias])
+  const existingAliases = useMemo(() =>
+    Object.entries(columnNameAliases).filter(([key, val]) => key !== value && val)
+      .map(([, val]) => val)
+  , [value, columnNameAliases])
+
+  useEffect(() => {
+    if (columnNameAliases[value] !== debouncedAlias &&
+      !existingAliases.includes(debouncedAlias)) {
+      userUpdate({ columnNameAliases: { [value]: debouncedAlias } })
+    }
+  }, [userUpdate, value, debouncedAlias, columnNameAliases, existingAliases])
+
+  const aliasError = useMemo(() => Boolean(value && debouncedAlias &&
+    existingAliases.includes(debouncedAlias)),
+  [value, existingAliases, debouncedAlias])
 
   return (
     <TextField
@@ -26,6 +40,8 @@ const ColumnAliasControls = ({ value, disabled }) => {
       inputProps={{ placeholder: 'Column title alias' }}
       onChange={setAlias}
       {...{ disabled }}
+      error={aliasError}
+      helperText={aliasError && 'Alias is already in use!'}
     />
   )
 }
