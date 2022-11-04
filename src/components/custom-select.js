2209c5c4d2c8ee92b392c62c74e7868fcb8e3f37
Erika Szabo <erika.szabo.geo@gmail.com>
2022-11-04 10:18:38 -0400

    CustomSelect - add id to root class

--- src/components/custom-select.js
+++ src/components/custom-select.js
@@ -4,7 +4,7 @@ import { DropdownSelect, Icons } from '@eqworks/lumen-labs'
 
 
 export const DROPDOWN_SELECT_CLASSES = {
-  root: 'shadow-light-10 border-2 border-secondary-200 rounded-md',
+  root: 'lumen-dropdown-root shadow-light-10 border-2 border-secondary-200 rounded-md',
   button: 'tracking-widest border-none max-h-20 overflow-y-auto',
   menu: 'min-w-full z-50',
   content: 'children:fill-current children:text-secondary-500',
