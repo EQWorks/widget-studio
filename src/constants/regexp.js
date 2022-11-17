0443ca26dc9c7da6c52b07fcf6fed1231d22554e
Erika Szabo <erika.szabo.geo@gmail.com>
2022-11-17 13:36:51 -0500

    constants/regexp - add yearClientRegExp & yearQuarterClientRegExp & remove reportYMRegExp

--- src/constants/regexp.js
+++ src/constants/regexp.js
@@ -1,5 +1,7 @@
+export const yearClientRegExp = new RegExp(/[_][0-9]{4}[_][0-9]+$/g)
+
 export const yearMonthClientRegExp = new RegExp(/[_][0-9]{6}[_][0-9]+$/g)
 
-export const clientIdRegExp = new RegExp(/[_][0-9]+$/g)
+export const yearQuarterClientRegExp = new RegExp(/[_][0-9]{4}q[1-4][_][0-9]+$/g)
 
-export const reportYMRegExp = new RegExp(/[_][0-9]{6}[_]/g)
+export const clientIdRegExp = new RegExp(/[_][0-9]+$/g)
