c9c1478805499130c05e591938e44ee75e868e99
Runzhou Li (Leo) <runzhou.li@gmail.com>
2022-12-01 12:00:01 -0500

    stories/sample-data - split data into individual JSONs

---
+++ stories/sample-data/index.js
@@ -0,0 +1,34 @@
+import execution1591 from './execution-1591.json'
+import execution141 from './execution-141.json'
+import execution305 from './execution-305.json'
+import savedQuery187 from './saved-query-187.json'
+import savedQuery191 from './saved-query-191.json'
+import savedQuery217 from './saved-query-217.json'
+import execution913 from './execution-913.json'
+import execution1389 from './execution-1389.json'
+import execution1458 from './execution-1458.json'
+import execution2123 from './execution-2123.json'
+import manual2000 from './manual-2000.json'
+import manual2001 from './manual-2001.json'
+import manual3000 from './manual-3000.json'
+import manual4000 from './manual-4000.json'
+import manual5000 from './manual-5000.json'
+
+
+export default {
+  'Execution-1591': execution1591,
+  'Execution-141': execution141,
+  'Execution-305': execution305,
+  'Saved query-187': savedQuery187,
+  'Saved query-191': savedQuery191,
+  'Saved query-217': savedQuery217,
+  'Execution-913': execution913,
+  'Execution-1389': execution1389,
+  'Execution-1458': execution1458,
+  'Execution-2123': execution2123,
+  'Manual-2000': manual2000,
+  'Manual-2001': manual2001,
+  'Manual-3000': manual3000,
+  'Manual-4000': manual4000,
+  'Manual-5000': manual5000,
+}
