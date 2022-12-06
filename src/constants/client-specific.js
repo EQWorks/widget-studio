c6a4aecbedfe94b4963fc7dbc537e964468b2796
Erika Szabo <erika.szabo.geo@gmail.com>
2022-12-06 15:29:38 -0500

    constants - rename column names to match new Cox queries in client-specific, data-format, insights-data-categories

--- src/constants/client-specific.js
+++ src/constants/client-specific.js
@@ -1,5 +1,5 @@
 // TO DO: move this out to Cox app
-export const COX_CATEGORY_SEGMENTS = ['Top Spending Geography', 'Frequently Transacting Geography']
+export const COX_CATEGORY_SEGMENTS = ['Top Spending', 'Frequently Transacting']
 
 export const COX_XWI_KEY_ALIASES = {
   'Poi id': 'Dealer ID',
