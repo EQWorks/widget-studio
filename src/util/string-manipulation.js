2ad2449e88a0323ea171914f6bb81ff0988f0e43
Erika Szabo <erika.szabo.geo@gmail.com>
2022-12-06 15:53:44 -0500

    util/string-maipulation - remove all Cox specific formatting

--- src/util/string-manipulation.js
+++ src/util/string-manipulation.js
@@ -1,34 +1,5 @@
-export const STRING_REPLACE_DICT = {
-  'pop': 'Population',
-  'income_sources_total_income_avg': 'Average income',
-  'census_fam_couple_with_no_children': 'Couple - no children',
-  'census_fam_couple_with_children': 'Couple - with children',
-  'census_fam_person_not_in_census_family': 'Other',
-  'Top Spending Geography': 'Top Spending',
-  'Frequently Transacting Geography': 'Transaction Frequency',
-  'highest_education_secondary_education' : 'Secondary Education',
-  'highest_education_college_uni_below_bachelor' : 'Degree - Below Bachelor',
-  'highest_education_uni_advanced' : 'Degree - Masters or Higher',
-  'highest_education_apprenticeship_trades' : 'Apprenticeship Trades',
-  'highest_education_uni_bachelor' : 'Degree - Bachelors',
-  'highest_education_no_education' : 'No Education',
-}
-
-const HEADER_START = [
-  'pop_',
-  'census_fam_',
-]
-
-const customCleanUp = s => {
-  const header = HEADER_START.find(h => s?.toString().startsWith(h))
-  if (header && !STRING_REPLACE_DICT[s]) {
-    return s?.toString().replace(header, '').replace(/./, v => v.toUpperCase()).replaceAll('_', ' ')
-  }
-  return STRING_REPLACE_DICT[s]
-}
-
 export const cleanUp = s => (
-  customCleanUp(s) || s?.toString().replace(/_/g, ' ').replace(/./, v => v.toUpperCase())
+  s?.toString().replace(/_/g, ' ').replace(/./, v => v.toUpperCase())
 )
 
 const PREPOSITIONS = ['a', 'above', 'across', 'against', 'along', 'among', 'an', 'around', 'at',
