b32f26bda8c518ed5fe00c840e55a1ec793c3e5c
Erika Szabo <erika.szabo.geo@gmail.com>
2023-01-05 16:33:20 -0500

    constants/client-specific - remove COX_XWI_KEY_ALIASES

--- src/constants/client-specific.js
+++ src/constants/client-specific.js
@@ -1,6 +1,4 @@
-// TO DO: move this out to Cox app
-export const COX_CATEGORY_SEGMENTS = ['Top Spending', 'Frequently Transacting']
-
+// TO DO: remove this once column aliases are implemented for x-walking reports
 export const COX_XWI_KEY_ALIASES = {
   'Poi id': 'Dealer ID',
   'Target poi id': 'Competitor ID',
