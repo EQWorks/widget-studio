625f520116fb01add7a30967b92e719a30052a07
Hamaad Chughtai <Hamaad102@gmail.com>
2023-01-09 05:23:32 -0500

    goober - added shouldForwardProp to accept camel case props

--- src/index.js
+++ src/index.js
@@ -1,6 +1,15 @@
-export { default as WidgetManager } from './widget-manager'
-export { default as InsightsDataProvider } from './insights-data-provider'
+import { createElement } from 'react'
+import { setup } from 'goober'
+import { shouldForwardProp } from 'goober/should-forward-prop'
+
 import Widget from './widget'
 
+
+export { default as WidgetManager } from './widget-manager'
+export { default as InsightsDataProvider } from './insights-data-provider'
+
+setup(createElement, undefined, undefined, shouldForwardProp((prop) => {
+  return !['showLegend', 'legendPosition'].includes(prop)
+}))
 
 export default Widget
