e05098d0535d7d23928925b2575e7a560982c9f0
Erika Szabo <erika.szabo.geo@gmail.com>
2023-01-18 16:31:24 -0500

    controls/ColumnAliasControls - create new comp for list of column alias changes

--- src/controls/editor-mode/components/column-alias-controls.js
+++ src/controls/editor-mode/components/column-alias-controls.js
@@ -1,70 +1,106 @@
-import React, { useEffect, useState, useMemo } from 'react'
-import PropTypes from 'prop-types'
+import React, { useState, useMemo, createElement } from 'react'
 
-import { useDebounce } from 'use-debounce'
-import { TextField } from '@eqworks/lumen-labs'
-import { useStoreActions, useStoreState } from '../../../store'
+import { TextField, makeStyles, Icons, getTailwindConfigColor } from '@eqworks/lumen-labs'
 
+import { useStoreState, useStoreActions } from '../../../store'
+import WidgetControlCard from '../../shared/components/widget-control-card'
+import cardTypes from '../../../constants/card-types'
+
+
+const classes = makeStyles({
+  row: {
+    display: 'grid',
+    gridTemplateColumns: '50% 50%',
+    marginBottom: '0.25rem',
+    gap: '0.25rem',
+  },
+  columnName: {
+    display: 'flex',
+    gap: '0.5rem',
+    alignItems: 'center',
+    fontSize: '0.786rem',
+    color: getTailwindConfigColor('secondary-800'),
+    backgroundColor: getTailwindConfigColor('secondary-100'),
+    borderRadius: '0.25rem',
+    padding: '0.375rem',
+  },
+  colKey: {
+    overflow: 'hidden',
+  },
+})
 
 const textfieldClasses = Object.freeze({
   container: 'mt-0.5',
 })
 
-const ColumnAliasControls = ({ value, disabled }) => {
-  const userUpdate = useStoreActions((actions) => actions.userUpdate)
+const ColumnAliasControls = () => {
+  // const update = useStoreActions(actions => actions.update)
+  const userUpdate = useStoreActions(actions => actions.userUpdate)
+
+  // const valueKeys = useStoreState((state) => state.valueKeys)
+  const columnsAnalysis = useStoreState((state) => state.columnsAnalysis)
   const columnNameAliases = useStoreState((state) => state.columnNameAliases || {})
-  const aliasesReseted =  useStoreState((state) => state.aliasesReseted)
-  const [alias, setAlias] = useState(columnNameAliases[value])
-  const [debouncedAlias] = useDebounce(value ? alias : '', 300)
-  // indicates if we changed key alias in the current component through onChange
-  const [aliasChanged, setAliasChanged] = useState(false)
+  // const aliasesReseted =  useStoreState((state) => state.aliasesReseted)
+  // const widgetControlCardEdit = useStoreState((state) => state.widgetControlCardEdit)
 
-  useEffect(() => {
-    setAliasChanged(false)
-    setAlias(columnNameAliases[value] || '')
-  }, [aliasesReseted, columnNameAliases, value])
+  const [localColumnNameAliases, setLocalColumnNameAliases] = useState(columnNameAliases)
+  const [aliasError, setAliasError] = useState(false)
 
-  const existingAliases = useMemo(() =>
-    Object.entries(columnNameAliases).filter(([key, val]) => key !== value && val)
-      .map(([, val]) => val.toLowerCase())
-  , [value, columnNameAliases])
-
-  useEffect(() => {
-    if (!aliasesReseted && value && columnNameAliases[value] !== debouncedAlias &&
-      !existingAliases.includes(debouncedAlias) && aliasChanged) {
-      userUpdate({ columnNameAliases: { [value]: debouncedAlias } })
-    }
-  }, [userUpdate, value, debouncedAlias, columnNameAliases, existingAliases, aliasesReseted, aliasChanged])
-
-  const aliasError = useMemo(() => Boolean(value && alias &&
-    existingAliases.includes(alias.toLowerCase())),
-  [value, existingAliases, alias])
+  const dataColumns = useMemo(() => (
+    Object.entries(columnsAnalysis).map(([c, { Icon }]) => ({ key: c, icon: Icon }) )
+  ), [columnsAnalysis])
 
   return (
-    <TextField
-      classes={textfieldClasses}
-      size={'md'}
-      value={alias}
-      inputProps={{ placeholder: 'Column title alias' }}
-      onChange={(val) => {
-        userUpdate({ aliasesReseted: false })
-        setAliasChanged(true)
-        setAlias(val)
-      }}
-      {...{ disabled }}
-      error={aliasError}
-      helperText={aliasError && 'Alias is already in use!'}
-    />
+    <WidgetControlCard
+      title={'Column Alias Configuration'}
+      type={cardTypes.DOMAIN}
+      clear={() => userUpdate({
+        aliasesReseted: true,
+        columnNameAliases: {},
+        localColumnNameAliases: {},
+      })}
+    >
+      <>
+        {/* <>
+          <span className={classes.header}>
+            {createElement(Icons.column, { size: 'sm', className: classes.headerIcon })}
+            {'Columns'}
+          </span>
+          <span className={`${classes.header} ${classes.gridTwoColumns}`}>
+            {createElement(Icons.alias, { size: 'sm', className: classes.headerIcon })}
+            {'Alias'}
+          </span>
+        </> */}
+        {dataColumns?.map((col, i) => (
+          <div key={i} className={classes.row}>
+            <div className={classes.columnName}>
+              {createElement(col.icon, { size: 'sm' })}
+              <p className={classes.colKey}>{col.key}</p>
+            </div>
+            <TextField
+              classes={textfieldClasses}
+              size={'md'}
+              value={localColumnNameAliases[col.key]}
+              inputProps={{ placeholder: 'Column title alias' }}
+              onChange={(val) => {
+                if (Object.values(localColumnNameAliases).includes(val)) {
+                  setAliasError(true)
+                }
+                // userUpdate({ aliasesReseted: false })
+                setLocalColumnNameAliases({
+                  ...localColumnNameAliases,
+                  [col.key]: val || '',
+                })
+              }}
+              // {...{ disabled }}
+              error={aliasError}
+              helperText={aliasError && 'Alias is already in use!'}
+            />
+          </div>
+        ))}
+      </>
+    </WidgetControlCard>
   )
-}
-
-ColumnAliasControls.propTypes = {
-  value: PropTypes.string.isRequired,
-  disabled: PropTypes.bool,
-}
-
-ColumnAliasControls.defaultProps = {
-  disabled: false,
 }
 
 export default ColumnAliasControls
