a769beb4e0e5b1e0bb39529293f6f25703d3699e
Erika Szabo <erika.szabo.geo@gmail.com>
2023-01-24 18:11:04 -0500

    adapters/local - add types.TEXT adapter

--- src/view/adapter/adapters/local.js
+++ src/view/adapter/adapters/local.js
@@ -1,28 +1,34 @@
 import Table from '../../../components/table'
 import types from '../../../constants/types'
 import Stat from './local-components/stat'
+import Text from './local-components/text'
 
 
 export default {
   [types.STAT]: {
     component: Stat,
-    adapt: (data, { title, valueKeys, genericOptions, uniqueOptions, onAfterPlot, ...config }) => {
-      return ({
-        data,
-        title,
-        values: valueKeys,
-        formatData: config.formatDataFunctions,
-        genericOptions,
-        uniqueOptions,
-        onAfterPlot,
-      })
-    },
+    adapt: (data, { title, valueKeys, genericOptions, uniqueOptions, onAfterPlot, ...config }) => ({
+      data,
+      title,
+      values: valueKeys,
+      formatData: config.formatDataFunctions,
+      genericOptions,
+      uniqueOptions,
+      onAfterPlot,
+    }),
   },
   [types.TABLE]: {
     component: Table,
     adapt: (data) => ({
       rows: data,
       showHeader: false,
+    }),
+  },
+  [types.TEXT]: {
+    component: Text,
+    adapt: (_, { valueKeys, genericOptions }) => ({
+      value: valueKeys[0]?.text,
+      genericOptions,
     }),
   },
 }
