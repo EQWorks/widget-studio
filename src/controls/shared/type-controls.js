445285a3f31b84205733d63fabf0836daeecfaad
Erika Szabo <erika.szabo.geo@gmail.com>
2023-01-24 18:06:12 -0500

    controls/WidgetTypeControls - adjust code for adding Text widget type

--- src/controls/shared/type-controls.js
+++ src/controls/shared/type-controls.js
@@ -77,7 +77,8 @@ const baseIconStyle = {
   display: 'flex',
   alignItems: 'center',
   justifyContent: 'center',
-  width: '2.813rem',
+  maxWidth: '2.813rem',
+  maxHeight: '4.2836875rem',
   '&> p': {
     transition: 'color 0.3s, background 0.3s',
     color: getTailwindConfigColor('secondary-600'),
@@ -131,16 +132,19 @@ const useStyles = ({ mode = modes.EDITOR }) => makeStyles(
     }
     : {
       outerContainer: {
-        display: 'flex',
+        display: 'grid',
+        gridTemplateColumns: 'repeat(5, 1fr)',
+        columnGap: '1.25rem',
+        rowGap: '1rem',
       },
       icon: {
         ...baseIconStyle,
         '--tw-shadow': '0px 1px 4px rgba(54, 111, 228, 0.1)',
         boxShadow: 'var(--tw-ring-offset-shadow, 0 0 #0000), var(--tw-ring-shadow, 0 0 #0000), var(--tw-shadow)',
         border: `0.3rem solid ${getTailwindConfigColor('secondary-50')}`,
-        width: '2.214rem',
+        maxWidth: '2.214rem',
+        maxHeight: '2.214rem',
         borderRadius: '0.5rem',
-        marginRight: '0.8rem',
         '&> svg': {
           ...baseIconStyle['&> svg'],
           padding: '0.3rem',
@@ -154,78 +158,105 @@ const useStyles = ({ mode = modes.EDITOR }) => makeStyles(
     }
 )
 
+const getPosition = ({ mode, type }) =>
+  mode === modes.EDITOR && [types.PIE, types.SCATTER, types.TEXT, types.TABLE].includes(type)
+  ||  mode === modes.QL && [types.PIE, types.TABLE].includes(type)
+    ? 'left'
+    : 'right'
+
+const getDescription = ({ type, noDataSource }) => {
+  if (noDataSource) {
+    return 'No data source is selected in this widget.'
+  }
+  if (type === types.MAP && !noDataSource) {
+    return 'No geo columns found in this dataset.'
+  }
+  if (type === types.TEXT && !noDataSource) {
+    return 'Select No Data in Data Source for this widget.'
+  }
+}
+
 const WidgetTypeControls = () => {
   const userUpdate = useStoreActions((actions) => actions.userUpdate)
-  const selectedType = useStoreState((state) => state.type)
-  const group = useStoreState((state) => state.group)
-  const domain = useStoreState((state) => state.domain)
-  const validMapGroupKeys = useStoreState((state) => state.validMapGroupKeys)
+
+  const {
+    type: selectedType,
+    group,
+    domain,
+    validMapGroupKeys,
+    dataReady,
+    dataIsXWIReport,
+    noDataSource,
+  } = useStoreState((state) => state)
   const mode = useStoreState((state) => state.ui.mode)
-  const dataReady = useStoreState((state) => state.dataReady)
-  const dataIsXWIReport = useStoreState((state) => state.dataIsXWIReport)
 
   const classes = useStyles({ mode })
 
   return (
-    <MutedBarrier mute={!dataReady} >
+    <MutedBarrier mute={!dataReady && !noDataSource} >
       <WidgetControlCard title='Widget Type'>
         <div className={classes.outerContainer}>
           {
-            Object.entries(typeInfo).map(([type, { mustGroup, icon: Icon, uniqueOptions }], i) => {
-              const disabled = type === types.MAP && !(validMapGroupKeys?.length || dataIsXWIReport)
-              let styles = [classes.icon]
-              disabled && styles.unshift(classes.disabledIcon)
-              type === selectedType && styles.unshift(classes.selectedIcon)
-              const renderIcon = (
-                <div className={styles.join(' ')}>
-                  <Icon />
-                  <p className={classes.label}>
-                    {type}
-                  </p>
-                </div>
-              )
-              return (
-                <button
-                  key={i}
-                  // variant='borderless'
-                  className={`${classes.button} ${disabled ? classes.disabledButton : ''}`}
-                  onClick={() => {
-                    if (!disabled && type !== selectedType) {
-                      userUpdate({
-                        group: mustGroup,
-                        ...(
-                          mustGroup !== group && {
-                            groupFilter: [],
-                            valueKeys: [],
-                            ...(domain?.key && { [domain.key]: null }),
-                          }
-                        ),
-                        chart2ValueKeys: [],
-                        type,
-                        uniqueOptions:
-                        Object.entries(uniqueOptions).reduce((acc, [k, { defaultValue }]) => {
-                          acc[k] = defaultValue
-                          return acc
-                        }, {}),
-                      })
+            Object.entries(typeInfo).filter(([type]) => mode === modes.EDITOR
+            || (mode === modes.QL && type !== types.TEXT))
+              .map(([type, { mustGroup, icon: Icon, uniqueOptions }], i) => {
+                const disabled = (type === types.MAP && !(validMapGroupKeys?.length || dataIsXWIReport))
+                  || (type !== types.TEXT && noDataSource) || (type === types.TEXT && !noDataSource)
+                let styles = [classes.icon]
+                disabled && styles.unshift(classes.disabledIcon)
+                type === selectedType && ((noDataSource && type === types.TEXT)
+                  || (!noDataSource && type !== types.TEXT)) &&
+                  styles.unshift(classes.selectedIcon)
+                const renderIcon = (
+                  <div className={styles.join(' ')}>
+                    <Icon />
+                    <p className={classes.label}>
+                      {type}
+                    </p>
+                  </div>
+                )
+                return (
+                  <button
+                    key={i}
+                    // variant='borderless'
+                    className={`${classes.button} ${disabled ? classes.disabledButton : ''}`}
+                    onClick={() => {
+                      if (!disabled && type !== selectedType) {
+                        userUpdate({
+                          group: mustGroup,
+                          ...(
+                            mustGroup !== group && {
+                              groupFilter: [],
+                              valueKeys: [],
+                              ...(domain?.key && { [domain.key]: null }),
+                            }
+                          ),
+                          chart2ValueKeys: [],
+                          type,
+                          uniqueOptions:
+                          Object.entries(uniqueOptions).reduce((acc, [k, { defaultValue }]) => {
+                            acc[k] = defaultValue
+                            return acc
+                          }, {}),
+                        })
+                      }
+                    }}
+                  >
+                    {
+                      disabled
+                        ? <Tooltip
+                          position={getPosition({ mode, type })}
+                          description={getDescription({ type, noDataSource })}
+                          {...(mode === modes.EDITOR && { width: '9rem' })}
+                        >
+                          {renderIcon}
+                        </Tooltip>
+                        : renderIcon
                     }
-                  }}
-                >
-                  {
-                    disabled
-                      ? <Tooltip
-                        position='right'
-                        description='No geo columns found in this dataset'
-                        {...(mode === modes.EDITOR && { width: '8rem' })}
-                      >
-                        {renderIcon}
-                      </Tooltip>
-                      : renderIcon
-                  }
-                </button>
+                  </button>
+                )
+              }
               )
-            }
-            )
           }
         </div >
       </WidgetControlCard>
