7411f59e730b462067caf34931d694adf782af46
kc-leung <chiu-leung@hotmail.com>
2023-02-03 16:14:27 -0500

    type-info - add  unique controls options for table widget

---
+++ src/constants/table-widget.js
@@ -0,0 +1,11 @@
+export const tableUniqueOptions = {
+  hidePagination: 'hidePagination',
+  headerTitle: 'headerTitle',
+  barColumns: 'barColumns',
+  borderType: 'borderType',
+  headerColor: 'headerColor',
+}
+
+export const headerColor = ['white', 'grey']
+
+export const borderType = ['none', 'horizontal', 'vertical', 'around']
--- src/view/adapter/adapters/local.js
+++ src/view/adapter/adapters/local.js
@@ -19,12 +19,33 @@ export default {
   },
   [types.TABLE]: {
     component: Table,
-    adapt: (data, {title}) => {
-      console.log('title: ', title)
+    adapt: (data, { title, uniqueOptions, genericOptions, ...config }) => {
+      const {
+        hidePagination,
+        headerTitle,
+        compactTable,
+        centerHeader,
+        barColumns,
+        borderType,
+        headerColor,
+      } = uniqueOptions
+      const { formatDataFunctions } = config
+
       return ({
         rows: data,
         showHeader: false,
-        barColumns: true
+        formatData: formatDataFunctions,
+        barColumns: barColumns.length ? barColumns : false,
+        barColumnsColor: genericOptions?.baseColor?.color1,
+        hidePagination,
+        headerTitle,
+        title,
+        defaultStyles: {
+          borderType,
+          headerColor,
+          compactTable,
+          centerHeader,
+        },
       })
     },
   },
