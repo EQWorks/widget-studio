c3b9f7b787987c9f9a52dc652300458dab0d0c8f
Erika Szabo <erika.szabo.geo@gmail.com>
2023-06-22 20:02:54 -0400

    .storybook/main - add webpack 5; @storybook/addon-actions to addons

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,9 +1,13 @@
 module.exports = {
+  core: {
+    builder: 'webpack5',
+  },
   "stories": [
     "../stories/**/*.stories.mdx",
     "../stories/**/*.stories.@(js|jsx|ts|tsx)"
   ],
   "addons": [
+    "@storybook/addon-actions",
     "@storybook/addon-links",
     "@storybook/addon-essentials"
   ],
