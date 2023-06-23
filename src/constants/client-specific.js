d9021bf56e132f050ae137e768f0ea3dac13e586
Erika Szabo <erika.szabo.geo@gmail.com>
2023-06-23 12:36:42 -0400

    constants/client-specific - remove COX specific constants

--- src/constants/client-specific.js
+++
@@ -1,7 +0,0 @@
-// TO DO: remove this once column aliases are implemented for x-walking reports
-export const COX_XWI_KEY_ALIASES = {
-  'poi_id': 'Dealer ID',
-  'target_poi_id': 'Competitor ID',
-  'poi_name': 'Dealer',
-  'target_poi_name': 'Competitor',
-}
